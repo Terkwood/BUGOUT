4fbb0add8fe446a6f092ea143b1e77ee22797158
Terkwood <metaterkhorn@gmail.com>
2019-06-11 10:23:27 -0400

    try table

--- judge/src/main/kotlin/Judge.kt
+++ judge/src/main/kotlin/Judge.kt
@@ -1,10 +1,10 @@
 import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.utils.Bytes
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.kstream.Consumed
-import org.apache.kafka.streams.kstream.KStream
-import org.apache.kafka.streams.kstream.Produced
+import org.apache.kafka.streams.kstream.*
+import org.apache.kafka.streams.state.KeyValueStore
 import java.util.*
 
 fun main() {
@@ -55,25 +55,23 @@ class Judge(private val brokers: String) {
                 jsonMapper.readValue(v, MoveMadeEv::class.java)
             }
 
-        val gameStatesJsonStream =
+        val gameStatesJsonStream: KTable<GameId, GameBoard> =
             moveMadeEventStream.groupByKey().aggregate<GameBoard>(
                 { GameBoard() },
                 { _, v, board ->
                     board.add(v)
-                }
-            ).toStream().map { key, value ->
-                KeyValue(
-                    key, jsonMapper
-                        .writeValueAsString(value)
-                )
-            }
-
-        gameStatesJsonStream.to(
-            GAME_STATES_TOPIC, Produced.with(
-                Serdes.UUID(),
-                Serdes.String()
+                }, Materialized.`as`<GameId, GameBoard, KeyValueStore<Bytes,
+                        ByteArray>>
+                    (GAME_STATES_TOPIC)
+                    .withValueSerde(
+                        Serdes
+                            .serdeFrom(
+                                GameBoardSerde.gameBoardSerializer,
+                                GameBoardSerde.gameBoardDeserializer
+                            )
+                    )
             )
-        )
+
 
         val topology = streamsBuilder.build()
 
