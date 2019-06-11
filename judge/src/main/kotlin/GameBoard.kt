87707dc0a872a6c47092c38405d2b9521218c53d
Terkwood <metaterkhorn@gmail.com>
2019-06-11 15:47:01 -0400

    add some help but try a type

--- judge/src/main/kotlin/GameBoard.kt
+++ judge/src/main/kotlin/GameBoard.kt
@@ -4,7 +4,9 @@ import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.common.serialization.Serializer
 
 class GameBoard {
+
     private val _board: MutableList<Move> = ArrayList()
+
     fun add(ev: MoveMadeEv): GameBoard {
         val move = Move(ev.player, ev.coord)
         if (!_board.contains(move))
@@ -12,13 +14,17 @@ class GameBoard {
 
         return this
     }
+
+    fun asByteArray(): ByteArray {
+        return jsonMapper.writeValueAsBytes(this)
+    }
 }
 
 private val gameBoardSerializer: Serializer<GameBoard> =
     JsonPOJOSerializer<GameBoard>()
 
 private val gameBoardDeserializer: Deserializer<GameBoard> =
-    JsonPOJODeserializer()
+    JsonPOJODeserializer<GameBoard>()
 
 val gameBoardSerde: Serde<GameBoard> =
     Serdes.serdeFrom(gameBoardSerializer, gameBoardDeserializer)
---
+++ judge/src/main/kotlin/GameBoardDeserializer.kt
@@ -0,0 +1,23 @@
+import org.apache.kafka.common.errors.SerializationException
+import org.apache.kafka.common.serialization.Deserializer
+
+class GameBoardDeserializer : Deserializer<GameBoard> {
+
+    override fun configure(configs: Map<String, *>, isKey: Boolean) {}
+
+    override fun close() {}
+
+    override fun deserialize(topic: String, bytes: ByteArray?): GameBoard? {
+        if (bytes == null) {
+            return null
+        }
+
+        try {
+            return jsonMapper.readValue(bytes, GameBoard::class.java)
+        } catch (e: RuntimeException) {
+            throw SerializationException("Error deserializing value", e)
+        }
+
+    }
+
+}
