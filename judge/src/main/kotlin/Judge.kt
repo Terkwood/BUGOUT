d2cbcf382abd648c82ec22ee0dd36c8a7c787a9d
Terkwood <metaterkhorn@gmail.com>
2019-06-11 10:54:01 -0400

    try something simple

--- judge/src/main/kotlin/Judge.kt
+++ judge/src/main/kotlin/Judge.kt
@@ -4,6 +4,7 @@ import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.kstream.Consumed
 import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.kstream.KTable
 import org.apache.kafka.streams.kstream.Produced
 import java.util.*
 
@@ -56,17 +57,23 @@ class Judge(private val brokers: String) {
             MOVE_MODE_EV_TOPIC,
             Produced.with(Serdes.UUID(), Serdes.String())
         )
-        
-        /* val gameStatesJsonStream: KTable<GameId, ArrayList<MoveMadeEv>> =
-             moveMadeEventStream.groupByKey().aggregate(
-                 { ArrayList<MoveMadeEv>(0) },
-                 { _, v, list ->
-                     list.add(v)
-                     list
-                 }
-             )
 
-         gameStatesJsonStream to GAME_STATES_TOPIC*/
+        val gameStatesTable: KTable<GameId, ArrayList<MoveMadeEv>> =
+            moveMadeEventStream.groupByKey().aggregate(
+                { ArrayList<MoveMadeEv>(0) },
+                { _, v, list ->
+                    list.add(v)
+                    list
+                }
+            )
+
+        val gameStatesJsonTable: KTable<GameId, String> =
+            gameStatesTable.mapValues { list ->
+                jsonMapper
+                    .writeValueAsString(list)
+            }
+
+        gameStatesJsonTable to GAME_STATES_TOPIC
 
         val topology = streamsBuilder.build()
 
