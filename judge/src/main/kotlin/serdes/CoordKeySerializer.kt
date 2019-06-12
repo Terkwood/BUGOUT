f5d6ff5199347833724383549a0ba01ad3e346b1
Terkwood <metaterkhorn@gmail.com>
2019-06-12 12:00:29 -0400

    add comment

--- judge/src/main/kotlin/serdes/CoordKeySerializer.kt
+++ judge/src/main/kotlin/serdes/CoordKeySerializer.kt
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializerProvider
 
-// Provide a JSON-prettified key string format for our Coord class
+/**
+ * Provides a JSON-prettified string for Coords
+ */
 internal class CoordKeySerializer : JsonSerializer<Coord>() {
     override fun serialize(
         value: Coord?,
