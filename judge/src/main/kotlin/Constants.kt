a07678165dd50e59ceb2e3b0d92fa5ecdf55adb2
Terkwood <metaterkhorn@gmail.com>
2019-06-12 09:58:52 -0400

    happily add replay topic

--- judge/src/main/kotlin/Constants.kt
+++ judge/src/main/kotlin/Constants.kt
@@ -4,5 +4,6 @@ const val MAKE_MOVE_CMD_TOPIC = "bugout-make-move-cmd"
 const val MOVE_MODE_EV_TOPIC = "bugout-move-made-ev"
 const val MOVE_REJECTED_EV_TOPIC = "bugout-move-rejected-ev"
 
+const val GAME_REPLAY_EV_TOPIC = "bugout-game-replay-ev"
 const val GAME_STATES_STORE_NAME = "bugout-game-states-store"
 const val GAME_STATES_TOPIC = "bugout-game-states"
