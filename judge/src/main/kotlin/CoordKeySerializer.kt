8795152cc4f509c4554a7b4ce279c28efb5001ea
Terkwood <metaterkhorn@gmail.com>
2019-06-12 11:42:35 -0400

    refactor files

---
+++ judge/src/main/kotlin/CoordKeySerializer.kt
@@ -0,0 +1,14 @@
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+
+internal class CoordKeySerializer : JsonSerializer<Coord>() {
+    override fun serialize(
+        value: Coord?,
+        gen: JsonGenerator?,
+        serializers: SerializerProvider?
+    ) {
+        if (value != null && gen != null)
+            gen.writeFieldName("${value.x}_${value.y}")
+    }
+}
--- judge/src/main/kotlin/JacksonInit.kt
+++ judge/src/main/kotlin/JacksonInit.kt
@@ -1,10 +1,8 @@
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.core.JsonProcessingException
-import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.databind.util.StdDateFormat
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import java.io.IOException
 
 
 val jsonMapper = ObjectMapper().apply {
@@ -25,30 +23,3 @@ val jsonMapper = ObjectMapper().apply {
     registerModule(simpleModule)
 }
 
-internal class CoordKeyDeserializer : KeyDeserializer() {
-    private val regex = Regex("(\\d+)_(\\d+)")
-    private fun coordFrom(str: String): Coord? {
-        val rm = regex.matchEntire(str)
-        val x = rm?.destructured?.component1()
-        val y = rm?.destructured?.component2()
-        if (x != null && y != null)
-            return Coord(x = x.toInt(), y = y.toInt())
-
-        return null
-    }
-
-    @Throws(IOException::class, JsonProcessingException::class)
-    override fun deserializeKey(key: String, ctxt: DeserializationContext):
-            Any? = coordFrom(key)
-}
-
-internal class CoordKeySerializer : JsonSerializer<Coord>() {
-    override fun serialize(
-        value: Coord?,
-        gen: JsonGenerator?,
-        serializers: SerializerProvider?
-    ) {
-        if (value != null && gen != null)
-            gen.writeFieldName("${value.x}_${value.y}")
-    }
-}
