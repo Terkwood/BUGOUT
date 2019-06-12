ca0e066a4ef20bd42be6fb8c1ad6377022c34027
Terkwood <metaterkhorn@gmail.com>
2019-06-12 19:19:31 -0400

    add test

--- judge/src/test/kotlin/CapturingTests.kt
+++ judge/src/test/kotlin/CapturingTests.kt
@@ -30,4 +30,30 @@ class CapturingTests {
         )
     }
 
+    @Test
+    fun edgeNeighbors() {
+        val pieces: MutableMap<Coord, Player> = hashMapOf(
+            Pair(Coord(0, 0), Player.BLACK),
+            Pair(Coord(1, 0), Player.BLACK),
+            Pair(Coord(2, 0), Player.BLACK),
+            Pair(Coord(0, 1), Player.WHITE),
+            Pair(Coord(1, 1), Player.WHITE),
+            Pair(Coord(2, 1), Player.BLACK),
+            Pair(Coord(0, 2), Player.WHITE),
+            Pair(Coord(1, 2), Player.WHITE),
+            Pair(Coord(2, 2), Player.WHITE),
+            Pair(Coord(4, 3), Player.BLACK),
+            Pair(Coord(1, 5), Player.WHITE)
+        )
+
+        val board = Board(pieces = pieces)
+        val expected = setOf(
+            Pair(Coord(0, 0), Player.BLACK),
+            Pair(Coord(1, 1), Player.WHITE),
+            Pair(Coord(0, 2), Player.WHITE)
+        )
+        assertEquals(
+            expected, neighbors(Coord(0, 1), board), "wrong neighbors"
+        )
+    }
 }
