38f4b23050d26f63859ee2d52cdda6892563122b
Terkwood <metaterkhorn@gmail.com>
2019-06-13 13:03:34 -0400

    stubby

--- gamestates-aggregator/src/main/kotlin/Aggregator.kt
+++ gamestates-aggregator/src/main/kotlin/Aggregator.kt
@@ -1,7 +1,14 @@
+import org.apache.kafka.streams.StreamsBuilder
+
 fun main() {
     Aggregator("kafka:9092").process()
 }
 
 class Aggregator(private val brokers: String) {
-    fun process() {}
+    fun process() {
+
+        val streamsBuilder = StreamsBuilder()
+
+        println("Hello World")
+    }
 }
