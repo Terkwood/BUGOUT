e24a2d89d248bec32eff9b5c10ca4eb334e12079
Terkwood <metaterkhorn@gmail.com>
2019-06-13 19:25:10 -0400

    script

--- kafkacat/feed.sh
+++ kafkacat/feed.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sleep 15
+sleep 20
 kafkacat -b kafka:9092 -t bugout-game-states -T -P -l msgs-game-states -K:
-kafkacat -b kafka:9092 -t bugout-make-move-cmd -T -P -l msgs-make-move-cmd -K:
 sleep 1
 echo 'a0b8d848-7c12-47fd-955f-c61c40d858af:{"gameId":"a0b8d848-7c12-47fd-955f-c61c40d858af","reqId":"0000b0e5-a943-491a-938a-19a35677a501", "player":"BLACK","coord":{"x":0,"y":0}}'| kafkacat -b kafka:9092 -t bugout-make-move-cmd -K: -P
 sleep 1
