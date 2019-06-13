3cb5856a5f4d261006787129365df795b020938f
Terkwood <metaterkhorn@gmail.com>
2019-06-12 21:54:55 -0400

    add test

--- judge/src/test/kotlin/CapturingTests.kt
+++ judge/src/test/kotlin/CapturingTests.kt
@@ -262,4 +262,29 @@ class CapturingTests {
 
         assertEquals(expected, actual, "not greedy enough")
     }
+
+    @Test
+    fun notTooGreedy() {
+        val pieces: MutableMap<Coord, Player> = hashMapOf(
+            Pair(Coord(0, 0), Player.BLACK),
+            Pair(Coord(1, 0), Player.BLACK),
+            Pair(Coord(2, 0), Player.BLACK),
+            Pair(Coord(0, 1), Player.WHITE),
+            Pair(Coord(1, 1), Player.WHITE),
+            Pair(Coord(2, 1), Player.BLACK),
+            Pair(Coord(0, 2), Player.WHITE),
+            Pair(Coord(1, 2), Player.WHITE),
+            Pair(Coord(2, 2), Player.WHITE),
+            Pair(Coord(4, 3), Player.BLACK),
+            Pair(Coord(1, 5), Player.WHITE)
+        )
+
+        val board = Board(pieces)
+
+        val actual = capturesFor(Player.WHITE, Coord(3, 1), board)
+
+        val expected: Set<Coord> = setOf()
+
+        assertEquals(expected, actual, "too greedy")
+    }
 }
