67c19f743f3687ddf9e8ec16d380ede2ba34f6cd
Terkwood <metaterkhorn@gmail.com>
2019-06-13 18:23:20 -0400

    script

--- kafkacat/feed.sh
+++ kafkacat/feed.sh
@@ -2,4 +2,5 @@
 
 kafkacat -b kafka:9092 -t bugout-game-states -T -P -l msgs-game-states -K:
 kafkacat -b kafka:9092 -t bugout-make-move-cmd -T -P -l msgs-make-move-cmd -K:
-tail -f /dev/null
+kafkacat -b kafka:9092 -t bugout-make-move-cmd -C -K:
+kafkacat -b kafka:9092 -t bugout-move-made-ev -C -K:
