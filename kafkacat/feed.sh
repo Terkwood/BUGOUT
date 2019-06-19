e15d011f407fbd8e9473428f168067ebce3cdba6
Terkwood <metaterkhorn@gmail.com>
2019-06-19 09:00:49 -0400

    hack script

--- kafkacat/feed.sh
+++ kafkacat/feed.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo 'a0b8d848-7c12-47fd-955f-c61c40d858af:{"board":{"pieces":{"0,0":"BLACK"},"size":19},"captures":{"black":0,"white":0},"turn":2,"playerUp":"WHITE"}' | kafkacat -b kafka:9092 -t bugout-game-states  -K: -P
+echo 'a0b8d848-7c12-47fd-955f-c61c40d858af:{"board":{"pieces":{},"size":19},"captures":{"black":0,"white":0},"turn":2,"playerUp":"WHITE"}' | kafkacat -b kafka:9092 -t bugout-game-states  -K: -P
 sleep 5
 echo 'a0b8d848-7c12-47fd-955f-c61c40d858af:{"gameId":"a0b8d848-7c12-47fd-955f-c61c40d858af","reqId":"0000b0e5-a943-491a-938a-19a35677a501", "player":"BLACK","coord":{"x":0,"y":0}}'| kafkacat -b kafka:9092 -t bugout-make-move-cmd -K: -P
-sleep 2
+sleep 5
 echo 'a0b8d848-7c12-47fd-955f-c61c40d858af:{"gameId":"a0b8d848-7c12-47fd-955f-c61c40d858af","reqId":"1000b0e5-a943-491a-938a-19a35677a501", "player":"WHITE","coord":{"x":1,"y":0}}'| kafkacat -b kafka:9092 -t bugout-make-move-cmd -K: -P
-sleep 2
+sleep 5
 echo 'a0b8d848-7c12-47fd-955f-c61c40d858af:{"gameId":"a0b8d848-7c12-47fd-955f-c61c40d858af","reqId":"2000b0e5-a943-491a-938a-19a35677a501", "player":"BLACK","coord":{"x":0,"y":1}}'| kafkacat -b kafka:9092 -t bugout-make-move-cmd -K: -P
-sleep 2
+sleep 5
 echo 'a0b8d848-7c12-47fd-955f-c61c40d858af:{"gameId":"a0b8d848-7c12-47fd-955f-c61c40d858af","reqId":"3000b0e5-a943-491a-938a-19a35677a501", "player":"WHITE","coord":{"x":1,"y":1}}'| kafkacat -b kafka:9092 -t bugout-make-move-cmd -K: -P
 
 kafkacat -b kafka:9092 -t bugout-move-made-ev -C -K: 
