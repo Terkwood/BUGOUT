d8a428022bf1378f3b301909351a80ac4d554ff6
Terkwood <metaterkhorn@gmail.com>
2019-06-27 15:54:03 -0400

    add models

--- gateway/src/model.rs
+++ gateway/src/model.rs
@@ -1,9 +1,29 @@
 use serde_derive::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Coord {
+    x: u16,
+    y: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Player {
+    BLACK,
+    WHITE,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(tag = "type")]
-enum MoveEvents {
-    MoveMadeEvent { reply_to: Uuid },
+pub enum MoveEvents {
+    MoveMade {
+        game_id: Uuid,
+        reply_to: Uuid,
+        event_id: Uuid,
+        player: Player,
+    },
+    MoveRejected {
+        reply_to: Uuid,
+    },
 }
