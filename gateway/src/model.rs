c5c0261893445b0307bcf447b47bd7d87e422a62
Terkwood <metaterkhorn@gmail.com>
2019-06-27 15:57:42 -0400

    extend model

--- gateway/src/model.rs
+++ gateway/src/model.rs
@@ -16,14 +16,32 @@ pub enum Player {
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(tag = "type")]
-pub enum MoveEvents {
+pub enum Commands {
+    MakeMove {
+        game_id: Uuid,
+        request_id: Uuid,
+        player: Player,
+        coord: Option<Coord>,
+    },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub enum Events {
     MoveMade {
         game_id: Uuid,
         reply_to: Uuid,
         event_id: Uuid,
         player: Player,
+        coord: Option<Coord>,
+        captured: Vec<Coord>,
     },
     MoveRejected {
+        game_id: Uuid,
         reply_to: Uuid,
+        event_id: Uuid,
+        player: Player,
+        coord: Coord,
     },
 }
