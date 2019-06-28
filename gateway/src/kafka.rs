93d8c9a690606861047b1554fcbb2ff592e79c08
Terkwood <metaterkhorn@gmail.com>
2019-06-28 19:31:17 -0400

    define consumer

--- gateway/src/kafka.rs
+++ gateway/src/kafka.rs
@@ -1,1 +1,20 @@
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
+use rdkafka::error::KafkaResult;
+use rdkafka::message::{Headers, Message};
+use rdkafka::util::get_rdkafka_version;
 
+/// Adapted from https://github.com/fede1024/rust-rdkafka/blob/master/examples/simple_consumer.rs
+fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create()
+        .expect("Consumer creation failed");
+}
