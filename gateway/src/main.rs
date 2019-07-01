a1e0ecf1a1872cdf8cfd450f6ce4f972b1f7df76
Terkwood <38859656+Terkwood@users.noreply.github.com>
2019-06-30 21:47:04 -0400

    Gateway: Refactor WsSession (#33)

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -19,27 +19,23 @@ use model::BugoutMessage;
 use websocket::WsSession;
 
 fn main() {
-    let (kafka_in, _kafka_out): (
+    let (kafka_in, kafka_out): (
         crossbeam::Sender<BugoutMessage>,
         crossbeam::Receiver<BugoutMessage>,
     ) = unbounded();
 
-    let (router_in, router_out): (
-        crossbeam::Sender<BugoutMessage>,
-        crossbeam::Receiver<BugoutMessage>,
-    ) = unbounded();
-
+    let kic = kafka_in.clone();
     thread::spawn(move || {
-        kafka::start(router_in);
+        kafka::start(kic);
     });
 
-    ws::listen("0.0.0.0:3012", |out| WsSession {
-        client_id: uuid::Uuid::new_v4(),
-        out,
-        ping_timeout: None,
-        expire_timeout: None,
-        kafka_in: kafka_in.clone(),
-        router_out: router_out.clone(),
+    ws::listen("0.0.0.0:3012", |out| {
+        WsSession::new(
+            uuid::Uuid::new_v4(),
+            out,
+            kafka_in.clone(),
+            kafka_out.clone(),
+        )
     })
     .unwrap();
 }
