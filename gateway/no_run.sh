319e04b2cf81d3cfa3c3e161b1e1f7dbc8c291d6
Terkwood <metaterkhorn@gmail.com>
2019-07-02 13:12:31 -0400

    add no_run script for gateway dev

---
+++ gateway/no_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Invoke this if you'd prefer to just sit around,
+# and do nothing, instead of actually turning on the
+# gateway when the container boots.
+
+# Useful when you're iterating on a branch.
+
+tail -f /dev/null
