3bd235bdd6fad9562c32d484d8e2dbaa16ed8932
Terkwood <metaterkhorn@gmail.com>
2019-07-08 17:19:36 -0400

    randomize client

--- gateway/examples/client.rs
+++ gateway/examples/client.rs
@@ -1,13 +1,16 @@
+extern crate rand;
 /// Adapted from https://raw.githubusercontent.com/housleyjk/ws-rs/master/examples/client.rs
 extern crate uuid;
 /// Simple WebSocket client with error handling. It is not necessary to setup logging, but doing
 /// so will allow you to see more details about the connection by using the RUST_LOG env variable.
 extern crate ws;
 
+use rand::Rng;
 use uuid::Uuid;
 use ws::{connect, CloseCode};
 
 fn main() {
+    let mut rng = rand::thread_rng();
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         panic!("You must specify a game ID as a command line argument")
@@ -28,7 +31,9 @@ fn main() {
 
         let game_id: Uuid = game_id_parsed.unwrap();
         let request_id = Uuid::new_v4();
-        let msg = format!("{{\"type\":\"MakeMove\",\"gameId\":\"{:?}\",\"reqId\":\"{:?}\",\"player\":\"WHITE\",\"coord\":{{\"x\":0,\"y\":0}}}}", game_id, request_id).to_string();
+
+        let (x, y) = (rng.gen_range(0,19), rng.gen_range(0,19)); 
+        let msg = format!("{{\"type\":\"MakeMove\",\"gameId\":\"{:?}\",\"reqId\":\"{:?}\",\"player\":\"WHITE\",\"coord\":{{\"x\":{},\"y\":{}}}}}", game_id, request_id, x, y).to_string();
 
         if out.send(msg).is_err() {
             println!("Websocket couldn't queue an initial message.")
