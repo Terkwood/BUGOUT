2044407286e6637ab3a88a5af3b7f6e3de520ad5
Terkwood <metaterkhorn@gmail.com>
2019-08-06 16:52:43 -0400

    fix build script

--- build-all-dc.sh
+++ build-all-dc.sh
@@ -8,7 +8,7 @@ docker/compose:1.13.0'
 
 # This script is useful for environments where you
 # do not have access to cargo, gradle etc
-docker-compose build judge
-docker-compose build changelog
-docker-compose build gateway
-docker-compose build kafkacat
+docker-compose -f dc-giant.yml build judge
+docker-compose -f dc-giant.yml  build changelog
+docker-compose -f dc-tiny.yml  build gateway
+docker-compose -f dc-giant.yml  build kafkacat
