e2a59be83a2b94454695db0a746876ac8ef800f6
Terkwood <metaterkhorn@gmail.com>
2019-08-08 14:24:30 -0400

    stubby

--- history-provider/src/main/kotlin/HistoryProvider.kt
+++ history-provider/src/main/kotlin/HistoryProvider.kt
@@ -1,13 +1,28 @@
+import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
+import java.util.*
+
+
+
 
 fun main() {
     HistoryProvider("kafka:9092").process()
 }
 
-class HistoryProvider (private val brokers: String) {
+class HistoryProvider(private val brokers: String) {
     fun process() {
         val streamsBuilder = StreamsBuilder()
 
-        // TODO
+        throw NotImplementedError()
+
+        val topology = streamsBuilder.build()
+
+        val props = Properties()
+        props["bootstrap.servers"] = brokers
+        props["application.id"] = "bugout-history-provider"
+        props["processing.guarantee"] = "exactly_once"
+        
+        val streams = KafkaStreams(topology, props)
+        streams.start()
     }
 }
