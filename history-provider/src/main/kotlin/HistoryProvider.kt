c24afd472b111a49ce3d6b5eb68767c4a284a7c5
Terkwood <metaterkhorn@gmail.com>
2019-08-08 14:20:55 -0400

    stub out HistoryProvider

--- history-provider/src/main/kotlin/HistoryProvider.kt
+++ history-provider/src/main/kotlin/HistoryProvider.kt
@@ -1,3 +1,13 @@
-fun main() {
+import org.apache.kafka.streams.StreamsBuilder
 
+fun main() {
+    HistoryProvider("kafka:9092").process()
+}
+
+class HistoryProvider (private val brokers: String) {
+    fun process() {
+        val streamsBuilder = StreamsBuilder()
+
+        // TODO
+    }
 }
