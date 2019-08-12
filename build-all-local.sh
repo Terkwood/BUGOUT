26c022b07e366c77b8663c5ef7bba9fa03388ade
Terkwood <metaterkhorn@gmail.com>
2019-08-12 16:30:56 -0400

    update docker - composes

--- build-all-local.sh
+++ build-all-local.sh
@@ -3,6 +3,7 @@
 docker-compose build judge
 docker-compose build changelog
 docker-compose build history-provider
+docker-compose build game-lobby
 docker-compose build kafkacat
 cd gateway && cargo clean && cd .. && docker-compose build gateway
 docker-compose build kafkacat
--- build-giant-dc.sh
+++ build-giant-dc.sh
@@ -2,5 +2,7 @@
 
 docker-compose -f dc-giant.yml build judge
 docker-compose -f dc-giant.yml build changelog
+docker-compose -f dc-giant.yml build game-lobby
+docker-compose -f dc-giant.yml build history-provider
 docker-compose -f dc-giant.yml build kafkacat
 
