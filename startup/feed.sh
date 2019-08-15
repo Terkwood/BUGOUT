6e9502bc53b9d062c2493c7677ad204c64d39d90
Terkwood <metaterkhorn@gmail.com>
2019-08-15 06:59:35 -0400

    add startup script

--- startup/feed.sh
+++ startup/feed.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+./wait-for-it.sh kafka:9092 -s -- sleep 16
+
+# make sure the game lobby has an empty entry
 echo 'ALL:{"games":[]}' | kafkacat -b kafka:9092 -t bugout-game-lobby -K: -P
