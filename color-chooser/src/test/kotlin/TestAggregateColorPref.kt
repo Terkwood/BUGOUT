a072face47c898bfab8f290fc973c3b3d2e775e3
Terkwood <metaterkhorn@gmail.com>
2019-08-19 06:54:42 -0400

    write test

--- color-chooser/src/test/kotlin/TestAggregateColorPref.kt
+++ color-chooser/src/test/kotlin/TestAggregateColorPref.kt
@@ -11,7 +11,47 @@ class TestAggregateColorPref {
     private val testDriver: TopologyTestDriver = setup()
 
     @BeforeAll
-    fun init() {}
+    fun init() {
+    }
+
+    @Test
+    fun test() {
+        val factory = ConsumerRecordFactory(UUIDSerializer(), StringSerializer())
+
+        val clientOne = ClientId(UUID.randomUUID())
+        val clientTwo = ClientId(UUID.randomUUID())
+        val gameId = GameId(UUID.randomUUID())
+
+        val clientOnePref = ChooseColorPref(clientOne, ColorPref.Any)
+        val clientTwoPref = ChooseColorPref(clientTwo, ColorPref.Black)
+
+        val gameReadyEvent = GameReady(gameId, Pair(clientOne, clientTwo), EventId(UUID.randomUUID()))
+
+        testDriver.pipeInput(
+            factory.create(
+                Topics.CHOOSE_COLOR_PREF,
+                clientOne.underlying,
+                jsonMapper.writeValueAsString(clientOnePref)
+            )
+        )
+
+        testDriver.pipeInput(
+            factory.create(
+                Topics.CHOOSE_COLOR_PREF,
+                clientTwo.underlying,
+                jsonMapper.writeValueAsString(clientTwoPref)
+            )
+        )
+
+        testDriver.pipeInput(
+            factory.create(
+                Topics.GAME_READY,
+                gameId.underlying,
+                jsonMapper.writeValueAsString(gameReadyEvent)
+            )
+        )
+
+    }
 
     @AfterAll
     fun tearDown() {
