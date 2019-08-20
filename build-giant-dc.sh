f894957f39d5196ad8af3fcb18ae8fcf19438ba2
Terkwood <metaterkhorn@gmail.com>
2019-08-20 11:04:38 -0400

    update build scripts

--- build-giant-dc.sh
+++ build-giant-dc.sh
@@ -3,6 +3,7 @@
 docker-compose -f dc-giant.yml build judge
 docker-compose -f dc-giant.yml build changelog
 docker-compose -f dc-giant.yml build game-lobby
+docker-compose -f dc-giant.yml build color-chooser
 docker-compose -f dc-giant.yml build history-provider
 docker-compose -f dc-giant.yml build kafkacat
 
