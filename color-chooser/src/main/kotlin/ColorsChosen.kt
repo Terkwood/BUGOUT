41640b5f04edc1f7a3cb617b027728a1abe56b92
Terkwood <metaterkhorn@gmail.com>
2019-08-20 07:35:05 -0400

    add color choice impl

--- color-chooser/src/main/kotlin/ColorsChosen.kt
+++ color-chooser/src/main/kotlin/ColorsChosen.kt
@@ -6,20 +6,40 @@ data class ColorsChosen(val gameId: GameId, val black: ClientId, val white: Clie
             return when {
                 isAny(x.first.colorPref) -> TODO("any first")
                 isAny(x.second.colorPref) -> TODO("any second")
-                x.first == x.second -> TODO("conflict")
-                // no conflict below
-                x.first.colorPref == ColorPref.Black ->
+                // no conflict
+                x.first.colorPref == ColorPref.Black && x.first != x.second ->
                     ColorsChosen(
                         gameId = x.first.gameId,
                         black = x.first.clientId,
-                        white = x.second.clientId)
-                x.first.colorPref == ColorPref.White ->
+                        white = x.second.clientId
+                    )
+                // no conflict
+                x.first.colorPref == ColorPref.White && x.first != x.second ->
                     ColorsChosen(
                         gameId = x.first.gameId,
                         white = x.first.clientId,
-                        black = x.second.clientId)
-                else -> TODO("else")
+                        black = x.second.clientId
+                    )
+                else -> TODO("conflict")
             }
         }
+
+        private fun random(): Color = TODO()
+
+        private fun force(cp: ColorPref): Color =
+            when (cp) {
+                ColorPref.Any -> random()
+                ColorPref.Black -> Color.Black
+                ColorPref.White -> Color.White
+            }
+
+
+        private fun other(c: Color) = when (c) {
+            Color.Black -> Color.White
+            Color.White -> Color.Black
+        }
+
     }
 }
+
+enum class Color { Black, White }
