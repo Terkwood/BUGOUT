e99d46d52674ca0b5bd661702b1b04a539ca7c3a
Terkwood <metaterkhorn@gmail.com>
2019-08-29 16:44:37 -0400

    Update topics and sleep time

--- game-lobby/run.sh
+++ game-lobby/run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# increasing the sleep vs other services since
-# this seems to hang  -- 2019-08-29
-./wait-for-it.sh kafka:9092 -s -- sleep 20
+./wait-for-it.sh kafka:9092 -s -- sleep 16
 java -jar bugout.game-lobby.jar
