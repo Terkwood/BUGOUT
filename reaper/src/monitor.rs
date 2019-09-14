fb5e363f0205330904116ded11cbb42db7178480
Terkwood <metaterkhorn@gmail.com>
2019-09-14 17:52:32 -0400

    Update monitor.rs

--- reaper/src/monitor.rs
+++ reaper/src/monitor.rs
@@ -1,6 +1,10 @@
-use crossbeam_channel::select;
+use std::time::Duration;
+
+use crossbeam_channel::{select, tick};
 
 use crate::model::*;
+
+const TICK_SECS: u64 = 5;
 
 /// Start a process which
 pub fn start(
@@ -8,9 +12,15 @@ pub fn start(
     activity_out: crossbeam::Receiver<KafkaActivity>,
 ) {
     let mut monitor = Monitor::new();
+    let ticker = tick(Duration::from_secs(TICK_SECS));
 
     loop {
         select! {
+            recv(ticker) -> _ => if monitor.is_system_dead_enough() {
+                if let Err(e) = shutdown_in.send(ShutdownCommand::new()) {
+                    println!("Failed to send shutdown command: {:?}", e)
+                }
+            },
             recv(activity_out) -> command =>
                 match command {
                     Ok(k) => monitor.push(k),
