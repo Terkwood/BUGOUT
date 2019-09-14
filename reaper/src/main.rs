efde4b6b46c9355ee955e47fdcc4babdf41526b3
Terkwood <metaterkhorn@gmail.com>
2019-09-14 15:54:11 -0400

    Update main.rs

--- reaper/src/main.rs
+++ reaper/src/main.rs
@@ -10,9 +10,22 @@ mod kafka;
 mod shutdown;
 mod topics;
 
+use crossbeam_channel::{unbounded, Receiver, Sender};
 use shutdown::shutdown;
 
+use crate::kafka::{KafkaActivity, ShutdownCommand};
+
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
+    println!("⚰️ {:<8} {}", NAME, VERSION);
+
+    let (activity_in, _): (Sender<KafkaActivity>, Receiver<KafkaActivity>) = unbounded();
+
+    let (_, shutdown_out): (Sender<ShutdownCommand>, Receiver<ShutdownCommand>) = unbounded();
+
     env::init();
-    shutdown();
+    kafka::start(activity_in, shutdown_out);
+    // TODO shutdown();
 }
