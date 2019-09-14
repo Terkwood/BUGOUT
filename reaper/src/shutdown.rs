4363ce646035ebbe42d5e857d53fdbdbf8ffe247
Terkwood <metaterkhorn@gmail.com>
2019-09-14 09:50:08 -0400

    send shutdown

--- reaper/src/shutdown.rs
+++ reaper/src/shutdown.rs
@@ -1,10 +1,16 @@
 use std::default::Default;
 
-use rusoto_core::Region;
-use rusoto_ec2::StopInstancesRequest;
-
 use crate::env::INSTANCE_NAME;
+use rusoto_core::Region;
+use rusoto_ec2::{Ec2, Ec2Client, StopInstancesRequest};
 
 pub fn shutdown() {
+    let client = Ec2Client::new(Region::UsEast1);
+    let request: StopInstancesRequest = StopInstancesRequest {
+        instance_ids: vec![INSTANCE_NAME.to_string()],
+        ..Default::default()
+    };
+    client.stop_instances(request);
+
     println!("Shutting down instance {}...", INSTANCE_NAME.to_string())
 }
