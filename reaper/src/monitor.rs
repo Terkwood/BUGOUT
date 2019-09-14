5b55e82c6654734fcfbbd19b8a9c467eab241667
Terkwood <metaterkhorn@gmail.com>
2019-09-14 17:47:31 -0400

    Update monitor.rs

--- reaper/src/monitor.rs
+++ reaper/src/monitor.rs
@@ -7,12 +7,14 @@ pub fn start(
     shutdown_in: crossbeam::Sender<ShutdownCommand>,
     activity_out: crossbeam::Receiver<KafkaActivity>,
 ) {
+    let mut monitor = Monitor::new();
+
     loop {
         select! {
             recv(activity_out) -> command =>
                 match command {
-                    Ok(_) => unimplemented!(),
-                    _ => unimplemented!(),
+                    Ok(k) => monitor.push(k),
+                    Err(e) => println!("Failed to select in monitor: {:?}", e),
                 }
         }
     }
@@ -20,6 +22,14 @@ pub fn start(
 
 struct Monitor(Vec<KafkaActivity>);
 impl Monitor {
+    pub fn new() -> Monitor {
+        Monitor(vec![])
+    }
+
+    pub fn push(&mut self, k: KafkaActivity) {
+        self.0.push(k)
+    }
+
     pub fn is_system_dead_enough(&mut self) -> bool {
         self.prune();
 
