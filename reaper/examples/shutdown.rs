d3619484d96f8be5188f152690a2c226fcb16b39
Terkwood <metaterkhorn@gmail.com>
2019-09-15 13:46:04 -0400

    Create shutdown.rs

---
+++ reaper/examples/shutdown.rs
@@ -0,0 +1,18 @@
+extern crate reaper;
+
+use crossbeam_channel::{unbounded, Receiver, Sender};
+
+use reaper::model::ShutdownCommand;
+use reaper::*;
+
+/// Shut down the instance based on the .env
+/// configuration for its name
+pub fn main() {
+    let (shutdown_in, shutdown_out): (Sender<ShutdownCommand>, Receiver<ShutdownCommand>) =
+        unbounded();
+
+    env::init();
+    shutdown::listen(shutdown_out);
+
+    shutdown_in.send(ShutdownCommand::new()).unwrap()
+}
