d0e8c21c0006c5e0a37bd142ece686f33af43fe6
Terkwood <metaterkhorn@gmail.com>
2019-09-16 15:06:49 -0400

    Update env.rs

--- reaper/src/env.rs
+++ reaper/src/env.rs
@@ -2,19 +2,23 @@ use dotenv::dotenv;
 use std::env;
 
 const ENV_TAG_NAME: &str = "INSTANCE_TAG_NAME";
+const ENV_AWS_ROLE_ARN: &str = "AWS_ROLE_ARN";
+const ENV_AWS_REGION: &str = "AWS_REGION";
 
 lazy_static! {
-    pub static ref INSTANCE_TAG_NAME: String = tag_name();
+    pub static ref INSTANCE_TAG_NAME: String = env_var(ENV_TAG_NAME);
+    pub static ref AWS_ROLE_ARN: String = env_var(ENV_AWS_ROLE_ARN);
+    pub static ref AWS_REGION: String = env_var(ENV_AWS_REGION);
 }
 
 pub fn init() {
     dotenv().ok();
 }
 
-fn tag_name() -> String {
-    if let Ok(i) = env::var(ENV_TAG_NAME) {
+fn env_var(v: &str) -> String {
+    if let Ok(i) = env::var(v) {
         i
     } else {
-        panic!("Specify desired instance nametag value in env")
+        panic!("You must specify {} in .env file", v)
     }
 }
