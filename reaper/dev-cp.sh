d59c97dbbc8c452a047465e8deb69015bafaf3f4
Terkwood <metaterkhorn@gmail.com>
2019-09-17 16:39:29 -0400

    Update dev-cp.sh

--- reaper/dev-cp.sh
+++ reaper/dev-cp.sh
@@ -3,5 +3,4 @@
 docker cp Cargo.toml bugout_reaper_1:/var/BUGOUT/reaper/.
 docker cp src bugout_reaper_1:/var/BUGOUT/reaper/src
 docker cp examples bugout_reaper_1:/var/BUGOUT/reaper/examples
-docker cp *.sh bugout_reaper_1:/var/BUGOUT/reaper/.
 
