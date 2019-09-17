aa53ddb19347d9321e65d07d2dfb6c51cb036e7d
Terkwood <metaterkhorn@gmail.com>
2019-09-17 15:28:41 -0400

    Update env.rs

--- reaper/src/env.rs
+++ reaper/src/env.rs
@@ -1,11 +1,17 @@
 use dotenv::dotenv;
 use std::env;
 
+const ENV_ALLOWED_IDLE_SECS: &str = "ALLOWED_IDLE_SECS";
 const ENV_TAG_NAME: &str = "INSTANCE_TAG_NAME";
 const ENV_AWS_REGION: &str = "AWS_REGION";
+
+const DEFAULT_ALLOWED_IDLE_SECS: i64 = 300;
 const DEFAULT_REGION: &str = "us-east-1";
 
 lazy_static! {
+    pub static ref ALLOWED_IDLE_SECS: i64 = env::var(ENV_ALLOWED_IDLE_SECS)
+        .map(|s| s.parse::<i64>().unwrap_or(DEFAULT_ALLOWED_IDLE_SECS))
+        .unwrap_or(DEFAULT_ALLOWED_IDLE_SECS);
     pub static ref INSTANCE_TAG_NAME: String = if let Ok(i) = env::var(ENV_TAG_NAME) {
         i
     } else {
