75721cc2b5e52cd71699f8181b0b55ef13d70a98
Terkwood <metaterkhorn@gmail.com>
2019-09-20 12:47:20 -0400

    Update run.sh

--- reaper/run.sh
+++ reaper/run.sh
@@ -2,5 +2,5 @@
 
 # Give it some extra time to let the rest of the system start
 # up and process messages
-./wait-for-it.sh kafka:9092 -s -- sleep 60
+./wait-for-it.sh kafka:9092 -s -- sleep 300
 reaper
