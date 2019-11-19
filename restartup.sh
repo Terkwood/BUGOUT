2a26bdc4a307c9b921dcc336b37585db3dcf098c
Terkwood <metaterkhorn@gmail.com>
2019-11-19 14:41:09 -0500

    Update restartup.sh

--- restartup.sh
+++ restartup.sh
@@ -2,10 +2,12 @@
 
 docker-compose down && \
     docker-compose up -d && \
+    echo "Sleep 30s" && \
     sleep 30 && \
     docker-compose restart game-lobby && \
     docker-compose restart color-chooser && \
     docker-compose restart history-provider && \
+    echo "Sleep 15s" && \
     sleep 15 && \
     docker-compose restart judge && \
     docker-compose restart changelog
