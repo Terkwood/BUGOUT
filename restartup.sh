679e848b6e7a4c4d791b24e3f5b65002ee30cae3
Terkwood <metaterkhorn@gmail.com>
2019-11-19 14:40:19 -0500

    Update restartup.sh

--- restartup.sh
+++ restartup.sh
@@ -3,8 +3,9 @@
 docker-compose down && \
     docker-compose up -d && \
     sleep 30 && \
-    docker-compose restart judge && \
-    docker-compose restart changelog && \
     docker-compose restart game-lobby && \
     docker-compose restart color-chooser && \
-    docker-compose restart history-provider
+    docker-compose restart history-provider && \
+    sleep 15 && \
+    docker-compose restart judge && \
+    docker-compose restart changelog
