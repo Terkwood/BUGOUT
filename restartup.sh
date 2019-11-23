63f40a3caab60ee9406f111cad9358a808993f31
Terkwood <metaterkhorn@gmail.com>
2019-11-23 17:10:47 -0500

    trim

--- restartup.sh
+++ restartup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-docker-compose down && \
-    docker-compose up -d && \
-    echo "Sleep 60s" && \
-    sleep 60 && \
-    docker-compose restart game-lobby && \
-    docker-compose restart color-chooser && \
-    docker-compose restart history-provider && \
-    docker-compose restart judge && \
-    docker-compose restart changelog
+docker-compose restart game-lobby && \
+docker-compose restart color-chooser && \
+docker-compose restart history-provider && \
+docker-compose restart judge && \
+docker-compose restart changelog
