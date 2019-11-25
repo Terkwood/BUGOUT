5dffea8b12f7fa435f5e6f988f01c86977dc41fe
Terkwood <metaterkhorn@gmail.com>
2019-11-25 16:17:01 -0500

    Remove dead script

--- restartup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker-compose restart game-lobby && \
-docker-compose restart color-chooser && \
-docker-compose restart history-provider && \
-docker-compose restart judge && \
-docker-compose restart changelog
