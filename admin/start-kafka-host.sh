213bd10e696a085de62056bcc2b92aaad5c9cfba
Terkwood <metaterkhorn@gmail.com>
2019-11-25 11:30:27 -0500

    Update start-kafka-host.sh

--- admin/start-kafka-host.sh
+++ admin/start-kafka-host.sh
@@ -2,4 +2,6 @@
 
 export HOSTNAME=$HOSTNAME
 echo "Determine KAFKA_ADVERTISED_LISTENERS using env HOSTNAME: $HOSTNAME"
-/opt/bin/docker-compose down && /opt/bin/docker-compose -f /home/core/BUGOUT/dc-giant.yml up
+cd /home/core/BUGOUT
+echo $PWD
+/opt/bin/docker-compose down && /opt/bin/docker-compose -f dc-giant.yml up
