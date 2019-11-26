068b900aff235ee951219efa8c509731bb992c21
Terkwood <metaterkhorn@gmail.com>
2019-11-26 18:38:05 -0500

    trim

--- admin/start-gateway-host.sh
+++ admin/start-gateway-host.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd /home/core/BUGOUT
-echo $PWD
 /opt/bin/docker-compose down && /opt/bin/docker-compose -f dc-tiny.yml up -d
--- admin/start-kafka-host.sh
+++ admin/start-kafka-host.sh
@@ -3,5 +3,4 @@
 export HOSTNAME=$HOSTNAME
 echo "Determine KAFKA_ADVERTISED_LISTENERS using env HOSTNAME: $HOSTNAME"
 cd /home/core/BUGOUT
-echo $PWD
 /opt/bin/docker-compose down && /opt/bin/docker-compose -f dc-giant.yml up -d
