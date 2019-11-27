1ceb97da5ac5a06a50d148f128666c831c182428
Terkwood <metaterkhorn@gmail.com>
2019-11-27 09:06:48 -0500

    Update idle_status.rs

--- gateway/src/idle_status.rs
+++ gateway/src/idle_status.rs
@@ -1,8 +1,12 @@
 use chrono::predule::*;
 
 /// The running status of an expensive container host
+/// 
+/// - Idle (since when)
+/// - Booting (an optional message indicating what's going on)
+/// - Awake (you may proceed to have fun)
 pub enum IdleStatus {
     Idle(DateTime<Utc>),
-    Booting(String),
+    Booting(Option<String>),
     Awake
 }
