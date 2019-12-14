58a8103d2a0871ddb7b7720972fdced4c683bab6
Terkwood <metaterkhorn@gmail.com>
2019-12-14 16:08:00 -0500

    Update ec2_startup.rs

--- bugle/src/ec2_startup.rs
+++ bugle/src/ec2_startup.rs
@@ -1,7 +1,10 @@
+use crate::env::*;
+
 use chrono::prelude::*;
 use chrono::{DateTime, Utc};
 use r2d2_redis::redis;
 use redis::Commands;
+use std::str::FromStr;
 
 use rusoto_core::Region;
 use rusoto_ec2::{DescribeInstancesRequest, Ec2, Ec2Client, StopInstancesRequest, Tag};
@@ -45,5 +48,54 @@ pub fn startup(pool: &RedisPool) {
         None => go(pool),
         Some(t) if Utc::now().timestamp() - t.timestamp() > DELAY_SECS as i64 => go(pool),
         Some(_) => println!("pass"),
+    }
+}
+
+fn big_box_instance_id(client: &Ec2Client) -> Option<String> {
+    let desc_request: DescribeInstancesRequest = Default::default();
+
+    let mut instance_id: Option<String> = None;
+
+    match client.describe_instances(desc_request).sync() {
+        Ok(d) => {
+            if let Some(rs) = d.reservations {
+                for r in rs {
+                    if let Some(is) = r.instances {
+                        for i in is {
+                            let id = i.instance_id;
+                            if let Some(tags) = i.tags {
+                                if has_required_name(tags) {
+                                    instance_id = id
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Err(error) => println!("Error: {:?}", error),
+    }
+
+    instance_id
+}
+
+fn has_required_name(tags: Vec<Tag>) -> bool {
+    for tag in tags {
+        match tag {
+            Tag {
+                key: Some(tag_key),
+                value: Some(v),
+            } if tag_key == EC2_TAG_KEY && v == INSTANCE_TAG_NAME.to_string() => return true,
+            _ => (),
+        }
+    }
+
+    return false;
+}
+
+fn region() -> Region {
+    match Region::from_str(&AWS_REGION.to_string()) {
+        Ok(r) => r,
+        Err(_e) => panic!("Failed to set AWS region"),
     }
 }
