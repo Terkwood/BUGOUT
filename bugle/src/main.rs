c3455c741a554875e94a2310b564ed10d6e1b426
Terkwood <metaterkhorn@gmail.com>
2019-12-14 13:22:43 -0500

    Trim

--- bugle/src/main.rs
+++ bugle/src/main.rs
@@ -2,20 +2,10 @@ extern crate bugle;
 
 use bugle::subscriber;
 
-use serde_json;
-use uuid::Uuid;
-
 const NAME: &'static str = env!("CARGO_PKG_NAME");
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     println!("🔢 {:<8} {}", NAME, VERSION);
-    println!(
-        "📯 {}",
-        serde_json::to_string(&bugle::WakeUpEvent {
-            client_id: Uuid::new_v4()
-        })
-        .unwrap()
-    );
     subscriber::start()
 }
