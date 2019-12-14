be015297d421f47189c731bb39b79a2f353cff34
Terkwood <metaterkhorn@gmail.com>
2019-12-14 13:01:09 -0500

    fix script

--- bugle/dev-cp.sh
+++ bugle/dev-cp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#!/bin/bash
 
 docker cp Cargo.toml bugout_bugle_1:/var/BUGOUT/bugle/.
-docker cp src bugout_bugle_1:/var/BUGOUT/bugle/src
+docker cp src bugout_bugle_1:/var/BUGOUT/bugle/.
 
