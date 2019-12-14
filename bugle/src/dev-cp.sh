20139fe572ed3e0ceb300d5bfc1079ee71248911
Terkwood <metaterkhorn@gmail.com>
2019-12-14 12:40:45 -0500

    Add dev scripts

--- bugle/src/dev-cp.sh
+++ bugle/src/dev-cp.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+#!/bin/bash
+
+docker cp Cargo.toml bugout_reaper_1:/var/BUGOUT/bugle/.
+docker cp src bugout_reaper_1:/var/BUGOUT/bugle/src
+docker cp examples bugout_reaper_1:/var/BUGOUT/bugle/examples
+
