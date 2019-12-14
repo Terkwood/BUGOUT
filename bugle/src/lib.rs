ae7f4bee3627d6e454379fc91ba10528a90345f1
Terkwood <metaterkhorn@gmail.com>
2019-12-14 13:15:08 -0500

    Implement basic subscriber

--- bugle/src/lib.rs
+++ bugle/src/lib.rs
@@ -1,9 +1,20 @@
 extern crate r2d2_redis;
 extern crate redis;
+extern crate serde;
+extern crate serde_derive;
+extern crate serde_json;
+extern crate uuid;
 
 pub mod subscriber;
 
 mod env;
 
-#[derive(Copy, Clone)]
-pub struct WakeUp;
+use serde_derive::{Deserialize, Serialize};
+use uuid::Uuid;
+
+pub type ClientId = Uuid;
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+pub struct WakeUpEvent {
+    client_id: ClientId,
+}
--- bugle/src/subscriber.rs
+++ bugle/src/subscriber.rs
@@ -1,24 +1,28 @@
+use crate::WakeUpEvent;
+
 use r2d2_redis::{r2d2, RedisConnectionManager};
 
+const HOST_URL: &str = "redis://redis";
+const TOPIC: &str = "wakeup-ev";
+
 pub fn start() {
-    let manager = RedisConnectionManager::new("redis://redis").unwrap();
+    let manager = RedisConnectionManager::new(HOST_URL).unwrap();
     let pool = r2d2::Pool::builder().build(manager).unwrap();
 
     let mut sub_conn = pool.get().unwrap();
     let mut sub = sub_conn.as_pubsub();
-    let topic = "wakeup-ev";
-    sub.subscribe(topic).unwrap();
 
-    println!("Subscribed to redis channel: {}", topic);
+    sub.subscribe(TOPIC).unwrap();
+
+    println!("Subscribed to redis channel: {}", TOPIC);
 
     loop {
         if let Ok(msg) = sub.get_message() {
             let payload = msg.get_payload().unwrap_or("".to_string());
-            println!("{}", payload)
-            /*let revent: Result<String, _> = serde_json::from_str(&payload);
-            if let Ok(e) = revent {
-                tx.send(e).unwrap()
-            }*/
+            let event: Result<WakeUpEvent, _> = serde_json::from_str(&payload);
+            if let Ok(e) = event {
+                println!("{:?}", e);
+            }
         }
     }
 }
