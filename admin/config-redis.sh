112bbbdec45726810a6af50e74210302bb698e11
Terkwood <metaterkhorn@gmail.com>
2019-12-14 10:49:37 -0500

    Update config-redis.sh

--- admin/config-redis.sh
+++ admin/config-redis.sh
@@ -4,7 +4,7 @@
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 
 # Enable kernel mod overcommit_memory
-sysctl -w vm.overcommit_memory = 1
+sysctl -w vm.overcommit_memory=1
 
 # Raise TCP backlog setting
 sysctl -w net.core.somaxconn=65535
