6a6368bea7dc0b42b9f6aa8aab12ede31f2484ca
Terkwood <metaterkhorn@gmail.com>
2019-12-14 12:49:49 -0500

    add deps

--- bugle/src/subscriber.rs
+++ bugle/src/subscriber.rs
@@ -3,5 +3,21 @@ use redis;
 use crate::WakeUp;
 
 pub fn start() {
-    unimplemented!()
+    let redis_client = unimplemented!();
+    let mut sub_conn = redis_client.get_connection().unwrap();
+    let mut sub = sub_conn.as_pubsub();
+    let topic = unimplemented!();
+    sub.subscribe(topic).unwrap();
+
+    println!("Subscribed to redis channel: {}", topic);
+
+    loop {
+        if let Ok(msg) = sub.get_message() {
+            let payload = msg.get_payload().unwrap_or("".to_string());
+            /*let revent: Result<String, _> = serde_json::from_str(&payload);
+            if let Ok(e) = revent {
+                tx.send(e).unwrap()
+            }*/
+        }
+    }
 }
