e719c4822d16545db25dd2fd7d0187421c5eeacf
Terkwood <metaterkhorn@gmail.com>
2019-12-14 12:36:25 -0500

    stubby

--- bugle/src/main.rs
+++ bugle/src/main.rs
@@ -1,5 +1,9 @@
 extern crate bugle;
 
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
+    println!("🔢 {:<8} {}", NAME, VERSION);    
     println!("📯 WAKE UP PLEASE!");
 }
