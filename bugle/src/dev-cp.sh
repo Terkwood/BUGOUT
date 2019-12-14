a39f07e1258b24210599d4cb19b69f2d6701221c
Terkwood <metaterkhorn@gmail.com>
2019-12-14 12:41:58 -0500

    update script

--- bugle/src/dev-cp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#!/bin/bash
-
-docker cp Cargo.toml bugout_reaper_1:/var/BUGOUT/bugle/.
-docker cp src bugout_reaper_1:/var/BUGOUT/bugle/src
-docker cp examples bugout_reaper_1:/var/BUGOUT/bugle/examples
-
