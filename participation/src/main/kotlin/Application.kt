2d47c6d972a38da81f5b2e9d9ae8b16d0013680a
Terkwood <metaterkhorn@gmail.com>
2020-01-06 16:48:48 -0500

    Stub out participation service

---
+++ participation/src/main/kotlin/Application.kt
@@ -0,0 +1,55 @@
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.utils.Bytes
+import org.apache.kafka.streams.*
+import org.apache.kafka.streams.kstream.*
+import org.apache.kafka.streams.state.KeyValueStore
+import java.util.*
+
+const val BROKERS = "kafka:9092"
+
+fun main() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    Application(BROKERS).process()
+}
+
+class Application(private val brokers: String) {
+    fun process() {
+        val topology = build()
+
+        println(topology.describe())
+
+        val props = Properties()
+        props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = brokers
+        props[StreamsConfig.APPLICATION_ID_CONFIG] = "bugout-participation"
+        props[StreamsConfig.PROCESSING_GUARANTEE_CONFIG] = "exactly_once"
+
+        val streams = KafkaStreams(topology, props)
+
+        waitForTopics(TODO(), props)
+
+        streams.start()
+    }
+
+    fun build(): Topology { TODO() }
+
+    private fun waitForTopics(topics: Array<String>, props:
+    Properties) {
+        print("⏲ Waiting for topics ")
+        val client = AdminClient.create(props)
+
+        var topicsReady = false
+        while(!topicsReady) {
+            val found = client.listTopics().names().get()
+
+            val diff = topics.subtract(found.filterNotNull())
+
+            topicsReady = diff.isEmpty()
+
+            if (!topicsReady) Thread.sleep(333)
+            print(".")
+        }
+
+        println(" done! 🏁")
+    }
+}
