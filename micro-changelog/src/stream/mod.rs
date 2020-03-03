4c15595e188680ea7869ae958f3a82068b89cba9
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-03-02 19:57:00 -0500

    Process stream

--- micro-changelog/src/stream/mod.rs
+++ micro-changelog/src/stream/mod.rs
@@ -1,10 +1,13 @@
 pub mod topics;
 mod xread;
 
+use crate::repo::entry_id_repo::EntryIdType;
 use crate::repo::*;
+use crate::Components;
+use micro_model_moves::*;
+use redis_streams::XReadEntryId;
 pub use topics::StreamTopics;
 use xread::*;
-
 pub fn process(topics: StreamTopics, components: &crate::Components) {
     println!("Processing {:#?}", topics);
     loop {
@@ -13,9 +16,39 @@ pub fn process(topics: StreamTopics, components: &crate::Components) {
                 Ok(xrr) => {
                     for time_ordered_event in xrr {
                         match time_ordered_event {
-                            (entry_id, StreamData::MA(move_acc)) => todo!(),
-                            (entry_id, StreamData::GR(gr_ev)) => todo!(),
-                            (entry_id, StreamData::GS(game_id, gs)) => todo!(),
+                            (entry_id, StreamData::MA(move_acc)) => {
+                                process_move_accepted(entry_id, move_acc, &components)
+                            }
+                            (entry_id, StreamData::GR(gr_ev)) => {
+                                if let Err(e) = game_states_repo::write(
+                                    gr_ev.game_id,
+                                    GameState::default(), // TODO handicaps
+                                    &components,
+                                ) {
+                                    println!("error saving fresh game state {:#?}", e)
+                                } else {
+                                    if let Err(e) = entry_id_repo::update(
+                                        EntryIdType::GameReadyEvent,
+                                        entry_id,
+                                        &components,
+                                    ) {
+                                        println!("Error saving entry ID on game ready {:#?}", e)
+                                    }
+                                }
+                            }
+                            (entry_id, StreamData::GS(game_id, gs)) => {
+                                if let Err(e) = game_states_repo::write(game_id, gs, &components) {
+                                    println!("Error saving game state {:#?}", e)
+                                } else {
+                                    if let Err(e) = entry_id_repo::update(
+                                        EntryIdType::GameStateChangelog,
+                                        entry_id,
+                                        &components,
+                                    ) {
+                                        println!("Error saving entry ID for game state {:#?}", e)
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -24,5 +57,38 @@ pub fn process(topics: StreamTopics, components: &crate::Components) {
             Err(FetchErr::Deser) => println!("Unable to deserialize entry IDs"),
             Err(FetchErr::Redis(r)) => println!("Redis err {:#?}", r),
         }
+    }
+}
+
+fn process_move_accepted(entry_id: XReadEntryId, move_acc: MoveMade, components: &Components) {
+    let game_id = move_acc.game_id.clone();
+    let old_game_state = game_states_repo::fetch(&move_acc.game_id, &components);
+    let new_game_state = old_game_state.map(|mut og| {
+        og.turn += 1;
+        og.player_up = match move_acc.player {
+            Player::BLACK => Player::WHITE,
+            _ => Player::BLACK,
+        };
+        let caps = move_acc.captured.len() as u16;
+        match move_acc.player {
+            Player::BLACK => og.captures.black += caps,
+            Player::WHITE => og.captures.white += caps,
+        }
+        for c in &move_acc.captured {
+            og.board.pieces.remove(c);
+        }
+        if let Some(c) = &move_acc.coord {
+            og.board.pieces.insert(*c, move_acc.player);
+        }
+
+        og.moves.push(move_acc);
+        og
+    });
+    let state_saved = new_game_state.map(|gs| game_states_repo::write(game_id, gs, &components));
+    if let Ok(Ok(write_result)) = state_saved {
+        todo!("Announce to move made");
+        entry_id_repo::update(EntryIdType::MoveAcceptedEvent, entry_id, &components);
+    } else {
+        println!("Trouble processing game state {:#?}", state_saved)
     }
 }
