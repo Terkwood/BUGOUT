73db049f5da111dbfe379bbc493a8654d5d0bc47
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-03-12 17:49:30 -0400

    Add model

--- tinybrain/src/lib.rs
+++ tinybrain/src/lib.rs
@@ -1,1 +1,27 @@
+extern crate serde;
+extern crate serde_derive;
+extern crate serde_json;
 
+use serde_derive::Serialize;
+
+#[derive(Debug, Clone, Serialize)]
+pub struct Move(pub String, pub String);
+
+#[derive(Debug, Clone, Serialize)]
+pub struct Rules(pub String);
+#[derive(Debug, Clone, Serialize)]
+pub struct Komi(pub f32);
+
+#[derive(Debug, Clone, Serialize)]
+pub struct KataGoQuery {
+    pub id: String,
+    #[serde(rename = "initialStone")]
+    pub initial_stones: Vec<Move>,
+    pub moves: Vec<Move>,
+    pub rules: Rules,
+    pub komi: Komi,
+    #[serde(rename = "boardXSize")]
+    pub board_x_size: u16,
+    #[serde(rename = "boardYSize")]
+    pub board_y_size: u16,
+}
