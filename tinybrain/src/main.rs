31d0177a8078e680d8518f2c8794567594bcfdef
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-03-12 17:02:31 -0400

    Fix COPY

--- tinybrain/src/main.rs
+++ tinybrain/src/main.rs
@@ -1,13 +1,17 @@
 extern crate tinybrain;
 use std::error::Error;
-use std::io::prelude::*;
 use std::io::{BufRead, BufReader, Write};
 use std::process::{Command, Stdio};
+
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 static COMMAND: &'static str =
 "{\"id\":\"big_08\",\"initialStones\":[],\"moves\":[   [\"B\",\"D3\"],    [\"W\",\"Q4\"],   [\"B\",\"Q10\"],    [\"W\",\"Q16\"],   [\"B\",\"K16\"],   [\"W\",\"D17\"],   [\"B\",\"C7\"], [\"W\",\"C14\"],   [\"B\",\"E13\"], [\"W\",\"D4\"],   [\"B\",\"C4\"], [\"W\",\"E3\"],   [\"B\",\"D2\"], [\"W\",\"C5\"],   [\"B\",\"B5\"], [\"W\",\"B4\"],   [\"B\",\"C3\"]], \"rules\":\"tromp-taylor\",\"komi\":7.5,\"boardXSize\":19,\"boardYSize\":19}";
 
 fn main() {
+    println!("🔢 {:<8} {}", NAME, VERSION);
+
     let mut process = match Command::new("./katago")
         .arg("analysis")
         .arg("-model")
@@ -38,7 +42,5 @@ fn main() {
             Err(why) => panic!("couldn't read   stdout: {}", why.description()),
             Ok(_) => print!("  responded with:\n{}", s),
         }
-
-        std::thread::sleep(std::time::Duration::from_secs(60));
     }
 }
