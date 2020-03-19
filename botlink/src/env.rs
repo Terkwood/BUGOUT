c523cea992a6eba5e6592d956b4bb3686fca0af0
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-03-19 19:53:40 -0400

    Stub out websocket handling

--- botlink/src/env.rs
+++ botlink/src/env.rs
@@ -1,9 +1,14 @@
 use std::env;
 
 const ENV_AUTHORIZATION: &str = "AUTHORIZATION";
+const ENV_ADDRESS: &str = "ADDRESS";
 
+const DEFAULT_ADDRESS: &str = "127.0.0.1:3012";
 lazy_static! {
     pub static ref AUTHORIZATION: Option<String> = env::var(ENV_AUTHORIZATION).ok();
+    pub static ref ADDRESS: String = env::var(ENV_ADDRESS)
+        .unwrap_or(DEFAULT_ADDRESS.to_string())
+        .to_string();
 }
 
 pub fn init() {
--- botlink/src/websocket.rs
+++ botlink/src/websocket.rs
@@ -1,8 +1,36 @@
+use crate::env;
 use crossbeam_channel::{Receiver, Sender};
 use micro_model_bot::{ComputeMove, MoveComputed};
+use std::net::TcpListener;
+use std::thread;
+use tungstenite::accept_hdr;
+use tungstenite::handshake::server::{Request, Response};
+use tungstenite::http;
+use tungstenite::Message;
+use uuid::Uuid;
 
-pub fn listen(_opts: WSOpts) {
-    todo!("Auth, and everything else")
+pub fn listen(opts: WSOpts) {
+    let server = TcpListener::bind(&*env::ADDRESS).expect("WS bind");
+
+    for stream in server.incoming() {
+        let _move_computed_in = opts.move_computed_in.clone();
+        let _compute_move_out = opts.compute_move_out.clone();
+        thread::spawn(move || {
+            let callback = |req: &Request, _response: Response| {
+                println!("Received a new ws handshake");
+                println!("The request's path is: {}", req.uri().path());
+                println!("The request's headers are:");
+                for (ref header, _value) in req.headers() {
+                    println!("* {}", header);
+                    todo!("Auth")
+                }
+
+                todo!("Auth")
+            };
+            let mut _websocket = accept_hdr(stream.expect("stream"), callback);
+            todo!("The Rest")
+        });
+    }
 }
 
 pub struct WSOpts {
