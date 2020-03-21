bbcef328ee5b918dbd665a8100ce8953990eeb31
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-03-21 11:33:43 -0400

    tinybrain: Add read-eval-print loop  (#199)
    ...and use `log` macros instead of `println!`.

--- tinybrain/src/lib.rs
+++ tinybrain/src/lib.rs
@@ -1,15 +1,17 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate base64;
 extern crate bincode;
 extern crate dotenv;
+extern crate env_logger;
+extern crate http;
+extern crate log;
 extern crate micro_model_moves;
 extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 extern crate tungstenite;
 extern crate uuid;
-#[macro_use]
-extern crate lazy_static;
-extern crate http;
 
 pub mod env;
 mod err;
