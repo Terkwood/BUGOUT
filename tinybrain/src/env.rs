1c079bdd52aa12772fce900609139126f3e366bd
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-03-23 06:06:02 -0400

    tinybrain: Rename env var, loosen coord case (#201)
    * Fix env var name and coord case
    * Expand test

--- tinybrain/src/env.rs
+++ tinybrain/src/env.rs
@@ -1,14 +1,14 @@
 use dotenv::dotenv;
 use std::env;
 const ENV_AUTHORIZATION: &str = "AUTHORIZATION";
-const ENV_ROBOCALL_URL: &str = "ROBOCALL_URL";
+const ENV_BOTLINK_URL: &str = "BOTLINK_URL";
 
-const DEFAULT_ROBOCALL_URL: &str = "ws://127.0.0.1:3012";
+const DEFAULT_BOTLINK_URL: &str = "ws://127.0.0.1:3012";
 
 lazy_static! {
     pub static ref AUTHORIZATION: Option<String> = env::var(ENV_AUTHORIZATION).ok();
-    pub static ref ROBOCALL_URL: String =
-        env::var(ENV_ROBOCALL_URL).unwrap_or(DEFAULT_ROBOCALL_URL.to_string());
+    pub static ref BOTLINK_URL: String =
+        env::var(ENV_BOTLINK_URL).unwrap_or(DEFAULT_BOTLINK_URL.to_string());
 }
 
 pub fn init() {
