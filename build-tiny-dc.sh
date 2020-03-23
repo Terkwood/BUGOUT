813f0876dfd7e4fe58ed83e7dd5b8e6b41da8954
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-03-22 20:34:13 -0400

    update script

--- build-tiny-dc.sh
+++ build-tiny-dc.sh
@@ -4,3 +4,4 @@ docker-compose -f dc-tiny.yml build gateway
 docker-compose -f dc-tiny.yml build micro-judge
 docker-compose -f dc-tiny.yml build micro-changelog
 docker-compose -f dc-tiny.yml build bugle
+docker-compose -f dc-tiny.yml build botlink
