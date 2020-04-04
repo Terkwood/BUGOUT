49a21ce283de63ad5c600a150537a905afc68a62
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-04-04 18:52:49 -0400

    Trim detritus

--- gateway/src/backend/mod.rs
+++ gateway/src/backend/mod.rs
@@ -1,13 +1,7 @@
-pub mod client_repo;
-pub mod game_repo;
-pub mod session_repo;
-
 mod doubler;
-mod repo_err;
 mod start;
 
-pub use doubler::double_commands;
-pub use repo_err::*;
-pub use start::{start_all, BackendInitOptions};
 use crate::backend_events::{BackendEvents, KafkaShutdownEvent};
 use crate::idle_status::KafkaActivityObserved;
+pub use doubler::double_commands;
+pub use start::{start_all, BackendInitOptions};
--- gateway/src/redis_io/stream.rs
+++ gateway/src/redis_io/stream.rs
@@ -1,6 +1,7 @@
 use crate::backend_events::BackendEvents;
 use crossbeam_channel::Sender;
+use log::warn;
 
 pub fn process(_events_in: Sender<BackendEvents>) {
-    todo!()
+    warn!("Stream processing unimplemented")
 }
