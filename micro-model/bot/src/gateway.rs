0d0bd72a15a4d2506a0cf4a14b620ffb9a207724
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-04-05 14:06:47 -0400

    Add board_size to botlink model (#207)
    * Update bot model
    
    * Remove Cargo.lock
    
    * Add some serde tests

--- micro-model/bot/src/gateway.rs
+++ micro-model/bot/src/gateway.rs
@@ -5,15 +5,18 @@ use serde_derive::{Deserialize, Serialize};
 /// requests that robocall coordinate with
 /// tinybrain to generate moves for a given
 /// game ID and player.
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub struct AttachBot {
     pub game_id: GameId,
     pub player: Player,
+    pub board_size: Option<u8>,
 }
 
 /// This reply is sent once a bot is listening
 /// as a certain player in a certain game.
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub struct BotAttached {
     pub game_id: GameId,
     pub player: Player,
@@ -33,5 +36,47 @@ impl BotAttached {
     }
     pub fn serialize(&self) -> Result<Vec<u8>, std::boxed::Box<bincode::ErrorKind>> {
         Ok(bincode::serialize(&self)?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Player;
+    use uuid::Uuid;
+
+    #[test]
+    fn test_attach_bot_json() {
+        let expected = AttachBot {
+            game_id: GameId(Uuid::nil()),
+            player: Player::BLACK,
+            board_size: Some(9),
+        };
+        let json = serde_json::to_string(&expected).expect("to_string");
+        let actual: AttachBot = serde_json::from_str(&json).expect("from_str");
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_bot_attached_json() {
+        let expected = BotAttached {
+            game_id: GameId(Uuid::new_v4()),
+            player: Player::WHITE,
+        };
+        let json = serde_json::to_string(&expected).expect("to_string");
+        let actual: BotAttached = serde_json::from_str(&json).expect("from_str");
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_camel_case_attach_bot() {
+        let input = AttachBot {
+            game_id: GameId(Uuid::nil()),
+            player: Player::BLACK,
+            board_size: Some(19),
+        };
+        let json = serde_json::to_string(&input).expect("to_string");
+        assert!(json.contains("gameId"));
+        assert!(json.contains("boardSize"))
     }
 }
