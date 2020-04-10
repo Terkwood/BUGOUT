d88edbbb5c52f81f20de34eb73cdcdf83c483532
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-04-10 19:19:46 -0400

    tinybrain: use tokio_tungstenite for websocket (#214)

--- tinybrain/src/websocket/mod.rs
+++ tinybrain/src/websocket/mod.rs
@@ -1,53 +1,71 @@
-use crate::*;
-use crossbeam_channel::{select, Receiver, Sender};
-use http::Request;
-use log::{error, trace, warn};
-use tungstenite::util::NonBlockingResult;
-use tungstenite::{connect, Message};
 mod authorization;
 
-pub fn start(compute_move_in: Sender<ComputeMove>, move_computed_out: Receiver<MoveComputed>) {
-    let (mut socket, response) =
-        connect(create_request()).expect("cannot connect to robocall host");
-    trace!("Connected to botlink, http status: {}", response.status());
+use crate::*;
 
-    trace!("Headers follow:");
-    for (ref header, _value) in response.headers() {
-        trace!("* {}", header)
-    }
+use bincode;
+use crossbeam_channel::{Receiver, Sender};
+use future::{select, Either};
+use futures_util::{future, SinkExt, StreamExt};
+use http::Request;
+use log::{error, info, warn};
+use std::time::Duration;
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
+const WRITE_TICK_MS: u64 = 10;
+pub async fn start(
+    compute_move_in: Sender<ComputeMove>,
+    move_computed_out: Receiver<MoveComputed>,
+) {
+    let (socket, response) = connect_async(create_request())
+        .await
+        .expect("cannot connect to botlink host");
+    info!("Connected to botlink, http status: {}", response.status());
+
+    let (mut write, mut read) = socket.split();
+
+    let mut interval = tokio::time::interval(Duration::from_millis(WRITE_TICK_MS));
+    let mut read_msg_fut = read.next();
+    let mut write_tick_fut = interval.next();
     loop {
-        let incoming_data = socket.read_message().no_block();
-        match incoming_data {
-            Err(e) => error!("Error reading incoming data {:?}", e),
-            Ok(None) => trace!("Empty read in ws"),
-            Ok(Some(tungstenite::Message::Binary(data))) => {
-                let cm: Result<ComputeMove, _> = bincode::deserialize(&data);
-                match cm {
-                    Err(e) => error!("failed to deser compute move {:?}", e),
-                    Ok(compute_move) => {
-                        if let Err(e) = compute_move_in.send(compute_move) {
-                            error!("failed to send compute move {:?}", e)
+        match select(read_msg_fut, write_tick_fut).await {
+            Either::Left((msg, write_tick_fut_continue)) => match msg {
+                Some(msg) => {
+                    if let Ok(msg) = msg {
+                        match msg {
+                            Message::Binary(data) => {
+                                let cm: Result<ComputeMove, _> = bincode::deserialize(&data);
+                                match cm {
+                                    Err(e) => error!("failed to deser compute move {:?}", e),
+                                    Ok(compute_move) => {
+                                        if let Err(e) = compute_move_in.send(compute_move) {
+                                            error!("failed to send compute move {:?}", e)
+                                        }
+                                    }
+                                }
+                            }
+                            Message::Text(_) => warn!("Unexpected text data"),
+                            Message::Close(_) => break,
+                            _ => (), // PingPong
                         }
                     }
+
+                    write_tick_fut = write_tick_fut_continue;
+                    read_msg_fut = read.next();
                 }
-            }
-            Ok(_e) => warn!("requires binary"),
-        };
-        select! {
-            recv(move_computed_out) -> mc =>
-                match mc {
-                    Err(e) => error!("Error reading move_computed_out {:?}",e),
-                    Ok(move_computed) => {
-                        socket
-                            .write_message(
-                                Message::Binary(
-                                    bincode::serialize(&move_computed)
-                                        .expect("bincode move computed")
-                                    )
-                                ).expect("write websocket message");
-                            trace!("Wrote on socket")
-                        }
+                None => break, // ws stream terminated
+            },
+            Either::Right((_, read_msg_fut_continue)) => {
+                while let Ok(mc) = move_computed_out.try_recv() {
+                    if let Err(e) = write
+                        .send(Message::Binary(bincode::serialize(&mc).expect("ser")))
+                        .await
+                    {
+                        error!("write {}", e)
+                    }
+                }
+
+                read_msg_fut = read_msg_fut_continue;
+                write_tick_fut = interval.next();
             }
         }
     }
