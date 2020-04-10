bf2064bc8892748863ec9716384f2b89b8cb1ac2
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-04-10 10:51:56 -0400

    dedupe code

--- botlink/src/stream/write_moves.rs
+++ botlink/src/stream/write_moves.rs
@@ -1,31 +1,29 @@
 use super::xadd::XAdder;
-use crossbeam_channel::{select, Receiver};
+use crossbeam_channel::{select, Receiver, RecvError};
 use log::error;
 use micro_model_bot::MoveComputed;
 use std::sync::Arc;
+
 pub fn write_moves(move_computed_out: Receiver<MoveComputed>, xadder: Arc<dyn XAdder>) {
     // Blocking recv until one message comes into the channel.
     // Going into select! right away can cause spin lock if channel empty.
     let msg = move_computed_out.recv();
+    handle(msg, &xadder);
+
+    loop {
+        select! { recv(move_computed_out) -> msg =>
+            handle(msg, &xadder)
+        }
+    }
+}
+
+fn handle(msg: Result<MoveComputed, RecvError>, xadder: &Arc<dyn XAdder>) {
     match msg {
         Ok(MoveComputed(command)) => {
             if let Err(e) = xadder.xadd_make_move_command(command) {
                 error!("could not xadd move command : {:?}", e)
             }
         }
-        Err(e) => error!("block recv: {}", e),
-    }
-
-    loop {
-        select! { recv(move_computed_out) -> msg =>
-            match msg {
-                Ok(MoveComputed(command)) =>
-                    if let Err(e)=xadder.xadd_make_move_command(command) {
-                        error!("could not xadd move command : {:?}",e)
-                    }
-                Err(e) =>
-                    error!("loop recv: {}", e)
-            }
-        }
+        Err(e) => error!("recv: {}", e),
     }
 }
