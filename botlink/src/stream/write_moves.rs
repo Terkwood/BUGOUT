e33d9cf92e7700c396c39eeab86cecfc2f1b55c6
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-04-10 10:46:40 -0400

    Initial blocking recv

--- botlink/src/stream/write_moves.rs
+++ botlink/src/stream/write_moves.rs
@@ -4,13 +4,28 @@ use log::error;
 use micro_model_bot::MoveComputed;
 use std::sync::Arc;
 pub fn write_moves(move_computed_out: Receiver<MoveComputed>, xadder: Arc<dyn XAdder>) {
+    // Blocking recv until one message comes into the channel.
+    // Going into select! right away can cause spin lock if channel empty.
+    let msg = move_computed_out.recv();
+    match msg {
+        Ok(MoveComputed(command)) => {
+            if let Err(e) = xadder.xadd_make_move_command(command) {
+                error!("could not xadd move command : {:?}", e)
+            }
+        }
+        Err(e) => error!("block recv: {}", e),
+    }
+
     loop {
         select! { recv(move_computed_out) -> msg =>
-            if let Ok(MoveComputed(command)) = msg {
-                if let Err(e)=xadder.xadd_make_move_command(command) {
-                    error!("could not xadd move command : {:?}",e)
-                }
-            } // swallow RecvError since this channel may be empty at startup
+            match msg {
+                Ok(MoveComputed(command)) =>
+                    if let Err(e)=xadder.xadd_make_move_command(command) {
+                        error!("could not xadd move command : {:?}",e)
+                    }
+                Err(e) =>
+                    error!("loop recv: {}", e)
+            }
         }
     }
 }
