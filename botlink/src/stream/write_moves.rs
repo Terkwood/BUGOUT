709bc55533ac878588fce2f204dce7d9e0726ff9
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-04-20 17:58:48 -0400

    Stub out conversion

--- botlink/src/stream/write_moves.rs
+++ botlink/src/stream/write_moves.rs
@@ -8,12 +8,15 @@ pub fn write_moves(move_computed_out: Receiver<MoveComputed>, xadder: Arc<dyn XA
     loop {
         select! { recv(move_computed_out) -> msg =>
             match msg {
-                Ok(MoveComputed(command)) =>
+                Ok(MoveComputed { game_id, player, alphanum_coord }) => {
+                    let command = todo!("look up board size and convert y appropriately");
+
                     if let Err(e) = xadder.xadd_make_move_command(&command) {
                         error!("could not xadd move command : {:?}",e)
                     } else {
                         info!("🆗 {:?}", command)
                     }
+                }
                 Err(e) =>
                     error!("loop recv: {}", e)
             }
