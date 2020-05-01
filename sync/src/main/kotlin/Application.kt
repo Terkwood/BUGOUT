540c9573d741ca5ed4bd34881db548f920e8f7b7
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-04-30 20:47:01 -0400

    Run main

--- sync/src/main/kotlin/Application.kt
+++ sync/src/main/kotlin/Application.kt
@@ -1,2 +1,14 @@
-class Application {
+import java.util.*
+
+const val BROKERS = "kafka:9092"
+
+fun main() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    Application(BROKERS).process()
+}
+
+class Application(private val brokers: String) {
+    fun process() {
+        print("!")
+    }
 }
