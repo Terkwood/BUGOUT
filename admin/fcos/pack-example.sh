2e57203a0837df6adb04c108f63944845a22e58b
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-01 07:18:11 -0400

    Fail if no validation

--- admin/fcos/pack-example.sh
+++ admin/fcos/pack-example.sh
@@ -4,6 +4,6 @@ export VPC_ID="vpc-bbbbbbbb"
 export SUBNET_ID="subnet-aaaaaaaa"
 export DOTENV_SRC="./some-bugout-folder"
 export USER_DATA_FILE="some-gateway.ign"
-packer validate gateway-packer.json
-packer build gateway-packer.json
+packer validate gateway-packer.json && \
+  packer build gateway-packer.json
 
