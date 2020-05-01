3b53df0454066e3891bb77cc2012ccff06f7f668
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-01 06:51:42 -0400

    Update packers

--- admin/fcos/pack-example.sh
+++ admin/fcos/pack-example.sh
@@ -3,5 +3,5 @@
 export SUBNET_ID=subnet-deadbeef
 export VPC_ID=vpc-foobar
 packer validate some-packer.json
-ENV_SRC=dev-env USER_DATA_FILE=some.ign \
+DOTENV_SRC=some-bugout-folder USER_DATA_FILE=some.ign \
 	packer build some-packer.json
