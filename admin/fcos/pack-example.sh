a7180e01e1501328ee6914a9f825976907c63b83
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-01 07:06:28 -0400

    Update example

--- admin/fcos/pack-example.sh
+++ admin/fcos/pack-example.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export SUBNET_ID=subnet-deadbeef
-export VPC_ID=vpc-foobar
-packer validate some-packer.json
-DOTENV_SRC=some-bugout-folder USER_DATA_FILE=some.ign \
-	packer build some-packer.json
+export VPC_ID="vpc-bbbbbbbb"
+export SUBNET_ID="subnet-aaaaaaaa"
+export DOTENV_SRC="./some-bugout-folder"
+export USER_DATA_FILE="some-gateway.ign"
+packer validate gateway-packer.json
+packer build gateway-packer.json
+
