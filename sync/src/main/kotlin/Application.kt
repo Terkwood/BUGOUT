6d758d330f090de8016e4381118e2b8117a19c6c
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-02 17:01:04 -0400

    Use spotless

--- sync/src/main/kotlin/Application.kt
+++ sync/src/main/kotlin/Application.kt
@@ -1,3 +1,7 @@
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.Topology
 import java.util.*
 
 const val BROKERS = "kafka:9092"
@@ -9,6 +13,43 @@ fun main() {
 
 class Application(private val brokers: String) {
     fun process() {
-        print("!")
+        val topology = build()
+
+        println(topology.describe())
+
+        val props = Properties()
+        props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = brokers
+        props[StreamsConfig.APPLICATION_ID_CONFIG] = "bugout-participation"
+        props[StreamsConfig.PROCESSING_GUARANTEE_CONFIG] = "exactly_once"
+
+        val streams = KafkaStreams(topology, props)
+
+        waitForTopics(Topics.all, props)
+
+        streams.start()
+    }
+
+    fun build() : Topology {
+        TODO()
+    }
+
+    private fun waitForTopics(topics: Array<String>, props:
+    Properties) {
+        print("⏲ Waiting for topics ")
+        val client = AdminClient.create(props)
+
+        var topicsReady = false
+        while(!topicsReady) {
+            val found = client.listTopics().names().get()
+
+            val diff = topics.subtract(found.filterNotNull())
+
+            topicsReady = diff.isEmpty()
+
+            if (!topicsReady) Thread.sleep(333)
+            print(".")
+        }
+
+        println(" done! 🏁")
     }
 }
--- sync/src/main/kotlin/Topics.kt
+++ sync/src/main/kotlin/Topics.kt
@@ -1,6 +1,10 @@
-const val GAME_STATES_CHANGELOG="bugout-game-states-changelog"
-const val MAKE_MOVE_CMD="bugout-make-move-cmd"
-const val REQ_SYNC_CMD="bugout-req-sync-cmd"
-const val SYNC_REPLY_EV="bugout-sync-reply-ev"
+object Topics {
+    const val GAME_STATES_CHANGELOG = "bugout-game-states-changelog"
+    const val MAKE_MOVE_CMD = "bugout-make-move-cmd"
+    const val REQ_SYNC_CMD = "bugout-req-sync-cmd"
+    const val SYNC_REPLY_EV = "bugout-sync-reply-ev"
 
-const val LOCAL_GAME_STATES_STORE="bugout-sync-game-states"
+    const val LOCAL_GAME_STATES_STORE = "bugout-sync-game-states"
+
+    val all = arrayOf(GAME_STATES_CHANGELOG, MAKE_MOVE_CMD, REQ_SYNC_CMD, SYNC_REPLY_EV, LOCAL_GAME_STATES_STORE)
+}
