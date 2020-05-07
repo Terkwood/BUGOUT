060d48241a99e8f539776fafca92e5fe3ec3e4ea
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-07 19:15:48 -0400

    Change store name

--- judge/src/main/kotlin/Topics.kt
+++ judge/src/main/kotlin/Topics.kt
@@ -5,6 +5,6 @@ const val MOVE_REJECTED_EV_TOPIC = "bugout-move-rejected-ev"
 const val GAME_STATES_CHANGELOG_TOPIC = "bugout-game-states"
 const val GAME_STATES_STORE = "bugout-game-states-store-judge"
 
-const val MAKE_MOVE_DEDUP_STORE = "bugout-judge-make-move-cmd-dedup"
+const val MAKE_MOVE_DEDUP_STORE = "bugout-judge-make-move-cmd-dedup-store"
 
 val ALL_TOPICS = arrayOf(GAME_STATES_CHANGELOG_TOPIC, MAKE_MOVE_CMD_TOPIC, MOVE_ACCEPTED_EV_TOPIC, MOVE_REJECTED_EV_TOPIC)
