7969405e5ead43a7badd26ebe33d3180ce6990a8
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-08 11:31:16 -0400

    admin: parameterize launch region

--- admin/fcos/launch.sh
+++ admin/fcos/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws ec2 run-instances --launch-template LaunchTemplateId=$TEMPLATE_ID --image-id $AMI_ID --subnet-id $SUBNET_ID --placement AvailabilityZone=us-east-1a
+aws ec2 run-instances --launch-template LaunchTemplateId=$TEMPLATE_ID --image-id $AMI_ID --subnet-id $SUBNET_ID --placement AvailabilityZone=$AZ
