8397310ccd824b201ef2a5da8ac0535f6cbe7d8f
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-09 08:23:36 -0400

    Add tinybrain systemd startup script (#292)
    ...and an example.

---
+++ tinybrain/start-tinybrain.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# systemd can use this script to start tinybrain
+
+if [ -z "$TINYBRAIN_HOME" ]
+then
+	echo "Please specify TINYBRAIN_HOME in env"
+	exit 1
+fi
+
+# move to a location where we expect a .env
+# file and a compiled katago executable
+cd $TINYBRAIN_HOME
+target/release/tinybrain
