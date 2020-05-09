239e1b860ebb9dca222f48ba3c5a7cb52595df0e
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-09 08:11:45 -0400

    Try env

--- tinybrain/start-tinybrain.sh
+++ tinybrain/start-tinybrain.sh
@@ -2,13 +2,13 @@
 
 # systemd can use this script to start tinybrain
 
-if [ -z "$TINYBRAIN_USER" ]
+if [ -z "$TINYBRAIN_HOME" ]
 then
-	echo "Please specify TINYBRAIN_USER in env"
+	echo "Please specify TINYBRAIN_HOME in env"
 	exit 1
 fi
 
 # move to a location where we expect a .env
 # file and a compiled katago executable
-cd /home/$TINYBRAIN_USER/BUGOUT/tinybrain
+cd $TINYBRAIN_HOME/tinybrain
 tinybrain
