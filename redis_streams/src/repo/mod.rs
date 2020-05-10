a0411d760aec4ecab4e40ae3b4cd8fcb2523604c
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-09 20:03:37 -0400

    Trim function sig

--- redis_streams/src/repo/mod.rs
+++ redis_streams/src/repo/mod.rs
@@ -5,11 +5,11 @@ use std::collections::HashMap;
 
 pub fn fetch_all<A: Default>(
     pool: &Pool,
-    provide_key: Box<dyn Fn() -> String>,
+    redis_key: &str,
     deser: Box<dyn Fn(HashMap<String, String>) -> A>,
 ) -> Result<A, EntryIdRepoErr> {
     if let Ok(mut conn) = pool.get() {
-        let found: Result<HashMap<String, String>, _> = conn.hgetall(provide_key());
+        let found: Result<HashMap<String, String>, _> = conn.hgetall(redis_key);
         Ok(if let Ok(hash) = found {
             deser(hash)
         } else {
@@ -24,16 +24,12 @@ pub fn update<B>(
     entry_id_type: B,
     entry_id: XReadEntryId,
     pool: &Pool,
-    provide_key: Box<dyn Fn() -> String>,
+    redis_key: &str,
     hash_field: Box<dyn Fn(B) -> String>,
 ) -> Result<(), EntryIdRepoErr> {
     let mut conn = pool.get().expect("redis pool");
-    conn.hset(
-        provide_key(),
-        hash_field(entry_id_type),
-        entry_id.to_string(),
-    )
-    .map_err(|_| EntryIdRepoErr)
+    conn.hset(redis_key, hash_field(entry_id_type), entry_id.to_string())
+        .map_err(|_| EntryIdRepoErr)
 }
 
 pub struct EntryIdRepoErr;
