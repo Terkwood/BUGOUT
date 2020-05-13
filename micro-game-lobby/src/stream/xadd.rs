8e0d1d109e523203d8e6162c95ef278d2366a659
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-13 18:53:25 -0400

    Write XAdd impl

--- micro-game-lobby/src/stream/xadd.rs
+++ micro-game-lobby/src/stream/xadd.rs
@@ -1,20 +1,44 @@
 use crate::api::*;
+use crate::topics::*;
+use community_redis_streams::{StreamCommands, StreamMaxlen};
 use redis::Client;
+use std::collections::BTreeMap;
 use std::rc::Rc;
 
 pub trait XAdd {
     fn xadd(&self, data: StreamOutput) -> Result<(), XAddErr>;
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub enum XAddErr {
-    Some,
-    Other,
+    Redis(redis::RedisError),
+    Ser,
+    Conn,
 }
 
+const AUTO_ID: &str = "*";
+const MAP_KEY: &str = "data";
+const MAX_LEN: usize = 1000;
 impl XAdd for Rc<Client> {
-    fn xadd(&self, _data: StreamOutput) -> Result<(), XAddErr> {
-        todo!()
+    fn xadd(&self, data: StreamOutput) -> Result<(), XAddErr> {
+        let (key, bytes_result) = match data {
+            StreamOutput::GR(gr) => (GAME_READY, bincode::serialize(&gr)),
+            StreamOutput::PGR(p) => (PRIVATE_GAME_REJECTED, bincode::serialize(&p)),
+            StreamOutput::WFO(w) => (WAIT_FOR_OPPONENT, bincode::serialize(&w)),
+        };
+
+        if let Ok(bytes) = bytes_result {
+            let mut m: BTreeMap<&str, &[u8]> = BTreeMap::new();
+            m.insert(MAP_KEY, &bytes);
+            if let Ok(mut conn) = self.get_connection() {
+                conn.xadd_maxlen_map(key, StreamMaxlen::Aprrox(MAX_LEN), AUTO_ID, m)
+                    .map_err(|e| XAddErr::Redis(e))
+            } else {
+                Err(XAddErr::Conn)
+            }
+        } else {
+            Err(XAddErr::Ser)
+        }
     }
 }
 
