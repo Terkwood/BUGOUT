3bba5434df66b9ddb2d21615fe5ff83524cc6aea
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-17 17:41:49 -0400

    Set version

--- admin/cleanup-dev-env.ts
+++ admin/cleanup-dev-env.ts
@@ -1,6 +1,9 @@
-#!/usr/bin/env -S deno run --allow-run --allow-net
+#!/usr/bin/env -S deno run --allow-run --allow-net --allow-read --allow-env
 // SPDX-License-Identifier: MIT
 import { runOrExit, parseProcessOutput, awsEc2Cmd } from "./procs.ts";
+import { config as loadDotEnv } from "https://deno.land/x/dotenv@v0.3.0/mod.ts";
+
+console.log(loadDotEnv({ safe: true }));
 
 let described = await runOrExit(
   { cmd: awsEc2Cmd("describe-instances"), stdout: "piped" },
