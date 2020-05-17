654a4a554c9add342a3da53156115eae911e4b85
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-17 14:44:44 +0000

    Update example

--- tinybrain/start-tinybrain.sh
+++ tinybrain/start-tinybrain.sh
@@ -8,6 +8,14 @@ then
 	exit 1
 fi
 
+if [ -z "$TINYBRAIN_BIN" ]
+then
+	echo "Please specify TINYBRAIN_BIN (parent folder of executable) in env"
+	exit 1
+fi
+
+export PATH="$PATH:$TINYBRAIN_BIN"
+
 # sorry, but we want to give the network additional
 # time to sort itself out.
 sleep 20 
@@ -15,4 +23,4 @@ sleep 20
 # move to a location where we expect a .env
 # file and a compiled katago executable
 cd $TINYBRAIN_HOME
-target/release/tinybrain
+tinybrain
