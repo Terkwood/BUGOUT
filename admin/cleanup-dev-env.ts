6890333c37f6afe2f2e0977ee2099124627dde9f
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-17 22:20:30 -0400

    Print addresses

--- admin/cleanup-dev-env.ts
+++ admin/cleanup-dev-env.ts
@@ -5,14 +5,33 @@ import { config as loadDotEnv } from "https://deno.land/x/dotenv@v0.3.0/mod.ts";
 
 console.log(loadDotEnv({ safe: true }));
 
-let described = await runOrExit(
+const KEY_NAME = Deno.env.get("KEY_NAME");
+
+let instsDescd = runOrExit(
   { cmd: awsEc2Cmd("describe-instances"), stdout: "piped" },
 );
 
-const { Reservations } = await parseProcessOutput(described);
+let addrsDescd = runOrExit(
+  { cmd: awsEc2Cmd("describe-addresses"), stdout: "piped" },
+);
 
+const { Reservations } = await parseProcessOutput(await instsDescd);
+
+console.log("Instances:");
 for (let { Instances } of Reservations) {
   for (let { InstanceId, KeyName } of Instances) {
-    console.log(`Hello ${InstanceId} ${KeyName}`);
+    console.log(`\t${InstanceId}`);
+    if (KEY_NAME === KeyName) {
+      console.log(`\t...Clean up on ${KeyName}`);
+    }
   }
+}
+
+const { Addresses } = await parseProcessOutput(await addrsDescd);
+
+console.log("Addresses:");
+for (let { InstanceId, AllocationId, AssociationId } of Addresses) {
+  console.log(
+    `\t${InstanceId} has alloc ${AllocationId} and assoc ${AssociationId}`,
+  );
 }
