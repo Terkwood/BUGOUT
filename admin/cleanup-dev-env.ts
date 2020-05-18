30f3fe46ebee1c02fffeaa7faf111d30ca1bdaf4
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-17 22:31:02 -0400

    Load dotenv

--- admin/cleanup-dev-env.ts
+++ admin/cleanup-dev-env.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env -S deno run --allow-run --allow-net --allow-read --allow-env
 // SPDX-License-Identifier: MIT
 import { runOrExit, parseProcessOutput, awsEc2Cmd } from "./procs.ts";
-import { config as loadDotEnv } from "https://deno.land/x/dotenv@v0.3.0/mod.ts";
+import { config as loadEnv } from "https://deno.land/x/dotenv@v0.3.0/mod.ts";
 
-console.log(loadDotEnv({ safe: true }));
+console.log(loadEnv({ safe: true, export: true }));
 
 const KEY_NAME = Deno.env.get("KEY_NAME");
+
+console.log(`Acting on ${KEY_NAME}`);
 
 let instsDescd = runOrExit(
   { cmd: awsEc2Cmd("describe-instances"), stdout: "piped" },
@@ -18,11 +20,14 @@ let addrsDescd = runOrExit(
 const { Reservations } = await parseProcessOutput(await instsDescd);
 
 console.log("Instances:");
+
+let instancesToTerminate = [];
 for (let { Instances } of Reservations) {
   for (let { InstanceId, KeyName } of Instances) {
-    console.log(`\t${InstanceId}`);
+    console.log(`\t${InstanceId} ${KeyName}`);
     if (KEY_NAME === KeyName) {
       console.log(`\t...Clean up on ${KeyName}`);
+      instancesToTerminate.push(InstanceId);
     }
   }
 }
@@ -30,8 +35,16 @@ for (let { Instances } of Reservations) {
 const { Addresses } = await parseProcessOutput(await addrsDescd);
 
 console.log("Addresses:");
+let addressesToRelease = [];
 for (let { InstanceId, AllocationId, AssociationId } of Addresses) {
   console.log(
     `\t${InstanceId} has alloc ${AllocationId} and assoc ${AssociationId}`,
   );
+  if (instancesToTerminate.includes(InstanceId)) {
+    addressesToRelease.push({ AllocationId, AssociationId });
+  }
 }
+
+console.log("\n");
+console.log(`Instances to terminate: ${JSON.stringify(instancesToTerminate)}`);
+console.log(`Addresses to release  : ${JSON.stringify(addressesToRelease)}`);
