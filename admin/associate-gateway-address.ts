c3aa77e2ff990babbdd00fbff082470a3c3e4d69
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-31 19:09:39 -0400

    Add imports

--- admin/associate-gateway-address.ts
+++ admin/associate-gateway-address.ts
@@ -1,1 +1,5 @@
 #!/usr/bin/env -S deno run --allow-net --allow-run --allow-read --allow-env
+// SPDX-License-Identifier: MIT
+
+import { runOrExit, parseProcessOutput, awsEc2Cmd } from "./procs.ts";
+import { config as loadEnv } from "https://deno.land/x/dotenv@v0.3.0/mod.ts";
