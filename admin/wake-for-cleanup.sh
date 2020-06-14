279debbd844efb14901f508b037ae7c42913d60b
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-06-14 07:53:15 -0400

    Set time

--- admin/wake-for-cleanup.sh
+++ admin/wake-for-cleanup.sh
@@ -6,4 +6,4 @@
 # needs to be run as root
 # and YES you need the sudo, when it's in a crontab
 # don't ask why
-sudo rtcwake -m no -l -t $(date +%s -d 'today 23:00')
+sudo rtcwake -m no -l -t $(date +%s -d 'today 22:57')
