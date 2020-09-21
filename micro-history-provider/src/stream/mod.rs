6979a94d4f09eb80f56e2d88f2abd3e04c6fc8f8
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-21 14:30:30 -0400

    Add topics

--- micro-history-provider/src/stream/mod.rs
+++ micro-history-provider/src/stream/mod.rs
@@ -1,4 +1,6 @@
+mod topics;
 mod xadd;
 mod xread;
+
 pub use xadd::XAdd;
 pub use xread::XRead;
---
+++ micro-history-provider/src/stream/topics.rs
@@ -0,0 +1,3 @@
+pub const GAME_STATES_CHANGELOG_TOPIC: &str = "bugout-game-states";
+pub const PROVIDE_HISTORY_TOPIC: &str = "bugout-provide-history-cmd";
+pub const HISTORY_PROVIDED_TOPIC: &str = "bugout-history-provided-ev";
