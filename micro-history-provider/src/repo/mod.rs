76659f6b9dce6a84cc8bbaaa23eca130583fbb49
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-23 15:55:08 -0400

    impl redis history get

--- micro-history-provider/src/repo/mod.rs
+++ micro-history-provider/src/repo/mod.rs
@@ -1,18 +1,31 @@
 use crate::model::*;
-use redis::Client;
+use redis::{Client, Commands};
 use std::rc::Rc;
 pub trait HistoryRepo {
     fn get(&self, game_id: &GameId) -> Result<Option<Vec<Move>>, FetchErr>;
     fn put(&self, game_id: &GameId, moves: Vec<Move>) -> Result<(), WriteErr>;
 }
+
+pub struct FetchErr;
+pub struct WriteErr;
+
 impl HistoryRepo for Rc<Client> {
     fn get(&self, game_id: &GameId) -> Result<Option<Vec<Move>>, FetchErr> {
-        todo!()
+        if let Ok(mut conn) = self.get_connection() {
+            let data: Result<Vec<u8>, _> = conn.get(redis_key(game_id)).map_err(|_| FetchErr);
+
+            data.and_then(|bytes| bincode::deserialize(&bytes).map_err(|_| FetchErr))
+        } else {
+            Err(FetchErr)
+        }
     }
 
     fn put(&self, game_id: &GameId, moves: Vec<Move>) -> Result<(), WriteErr> {
-        todo!()
+        todo!();
+        todo!("ttl")
     }
 }
-pub struct FetchErr;
-pub struct WriteErr;
+
+fn redis_key(game_id: &GameId) -> String {
+    format!("/BUGOUT/micro_history_provider/history/{}", game_id.0)
+}
