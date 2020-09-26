f165f788b5cff52fed6c72b12c3cd2d5aafcd014
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-26 13:57:23 -0400

    nit

--- micro-color-chooser/src/service/game_color_prefs.rs
+++ micro-color-chooser/src/service/game_color_prefs.rs
@@ -4,9 +4,13 @@ use crate::model::*;
 use crate::repo::*;
 
 /// Call this when you receive a ChooseColorPref event
-
+/// It will provide an aggregated view of choices for that game,
+/// based on all available data from both session_game repo
+/// and prefs repo.
 pub fn by_session_id(session_id: &SessionId, repos: &Repos) -> Result<GameColorPref, FetchErr> {
-    todo!("aa")
+    todo!("when there is no link from session ID to game, it's empty");
+    todo!("when there is a link, but only one person's prefs registered, partial");
+    todo!("otherwise complete")
 }
 
 /// Call this when you receive a GameReady event.
@@ -14,7 +18,7 @@ pub fn by_session_id(session_id: &SessionId, repos: &Repos) -> Result<GameColorP
 /// based on all available data from both session_game repo
 /// and prefs repo.
 pub fn by_game_ready(game_ready: &GameReady, repos: &Repos) -> Result<GameColorPref, FetchErr> {
-    todo!("bb")
+    todo!("check session prefs")
 }
 
 #[cfg(test)]
