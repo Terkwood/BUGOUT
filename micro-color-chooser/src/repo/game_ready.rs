2510f0a7b04af100fd5ac1087ffa394f87df60d8
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-27 15:12:56 -0400

    refactor fn

--- micro-color-chooser/src/repo/game_ready.rs
+++ micro-color-chooser/src/repo/game_ready.rs
@@ -27,23 +27,22 @@ impl GameReadyRepo for Rc<Client> {
     }
 
     fn put(&self, game_ready: GameReady) -> Result<(), WriteErr> {
-        if let Ok(mut conn) = self.get_connection() {
+        let c = self.get_connection();
+        let s = bincode::serialize(&game_ready);
+        if let (Ok(mut conn), Ok(bytes)) = (c, s) {
             let first_key = redis_key(&game_ready.sessions.0);
             let second_key = redis_key(&game_ready.sessions.0);
-            if let Ok(bytes) = bincode::serialize(&game_ready) {
-                let first_done: Result<(), _> = conn.set(&first_key, bytes.clone());
-                if let Ok(_) = first_done {
-                    touch_ttl(&mut conn, &first_key)
-                }
-                let second_done: Result<(), _> = conn.set(&second_key, bytes);
-                if let Ok(_) = second_done {
-                    touch_ttl(&mut conn, &second_key)
-                }
 
-                first_done.and_then(|_| second_done).map_err(|_| WriteErr)
-            } else {
-                Err(WriteErr)
+            let first_done: Result<(), _> = conn.set(&first_key, bytes.clone());
+            if let Ok(_) = first_done {
+                touch_ttl(&mut conn, &first_key)
             }
+            let second_done: Result<(), _> = conn.set(&second_key, bytes);
+            if let Ok(_) = second_done {
+                touch_ttl(&mut conn, &second_key)
+            }
+
+            first_done.and_then(|_| second_done).map_err(|_| WriteErr)
         } else {
             Err(WriteErr)
         }
