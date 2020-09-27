18c2936939638c58a24173d8deced08e0ea1948f
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-27 14:53:34 -0400

    add touch_ttl

--- micro-color-chooser/src/repo/mod.rs
+++ micro-color-chooser/src/repo/mod.rs
@@ -5,7 +5,7 @@ pub use game_ready::*;
 pub use prefs::*;
 
 use crate::model::*;
-use redis::Client;
+use redis::{Client, Commands, Connection};
 use std::rc::Rc;
 
 #[derive(Debug)]
@@ -15,6 +15,12 @@ pub struct FetchErr;
 pub struct WriteErr;
 
 const EXPIRY_SECS: usize = 86400;
+
+pub fn touch_ttl(key: &str, conn: &mut Connection) -> Result<(), WriteErr> {
+    let exp: Result<(), _> = conn.expire(key, EXPIRY_SECS).map_err(|_| WriteErr);
+
+    exp
+}
 
 impl From<redis::RedisError> for FetchErr {
     fn from(_: redis::RedisError) -> Self {
