6d758f286d7b8aa6c378b91c4b7c7320b60524fd
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-27 15:19:32 -0400

    write xadd impl

--- micro-color-chooser/src/stream/xadd.rs
+++ micro-color-chooser/src/stream/xadd.rs
@@ -1,6 +1,9 @@
+use super::topics;
 use crate::api::ColorsChosen;
-use redis::Client;
+use redis::{streams::StreamMaxlen, Client, Commands};
+use std::collections::BTreeMap;
 use std::rc::Rc;
+
 pub trait XAdd {
     fn xadd(&self, data: ColorsChosen) -> Result<(), XAddErr>;
 }
@@ -11,8 +14,30 @@ pub enum XAddErr {
     Conn,
 }
 
+const AUTO_ID: &str = "*";
+const MAP_KEY: &str = "data";
+const MAX_LEN: usize = 1000;
+
 impl XAdd for Rc<Client> {
     fn xadd(&self, data: ColorsChosen) -> Result<(), XAddErr> {
-        todo!()
+        let ser_bytes_result = bincode::serialize(&data);
+
+        if let Ok(bytes) = ser_bytes_result {
+            let mut m: BTreeMap<&str, &[u8]> = BTreeMap::new();
+            m.insert(MAP_KEY, &bytes);
+            if let Ok(mut conn) = self.get_connection() {
+                conn.xadd_maxlen_map(
+                    topics::COLORS_CHOSEN,
+                    StreamMaxlen::Approx(MAX_LEN),
+                    AUTO_ID,
+                    m,
+                )
+                .map_err(|e| XAddErr::Redis(e))
+            } else {
+                Err(XAddErr::Conn)
+            }
+        } else {
+            Err(XAddErr::Ser)
+        }
     }
 }
