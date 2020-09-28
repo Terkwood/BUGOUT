74440549ec9b3de618198a7dc88b4a3a360837f8
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-28 13:44:43 -0400

    set fn args

--- micro-sync/src/sync.rs
+++ micro-sync/src/sync.rs
@@ -1,4 +1,10 @@
 use crate::api::ReqSync;
-pub fn is_client_ahead_by_one_turn(req_sync: &ReqSync) -> bool {
+use crate::model::{Move, Player};
+pub fn is_client_ahead_by_one_turn(
+    req_sync: &ReqSync,
+    system_history: Vec<Move>,
+    system_turn: u32,
+    system_player_up: Player,
+) -> bool {
     todo!()
 }
