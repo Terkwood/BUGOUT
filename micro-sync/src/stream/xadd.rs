60bd0a0ac5a2163e154ec94e4be6d9bcd1656432
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-29 10:31:30 -0400

    fill in xadd impls

--- micro-sync/src/stream/xadd.rs
+++ micro-sync/src/stream/xadd.rs
@@ -45,10 +45,41 @@ impl XAdd for Rc<Client> {
     }
 
     fn add_sync_reply(&self, data: SyncReply) -> Result<(), XAddErr> {
-        todo!()
+        let ser_bytes_result = bincode::serialize(&data);
+
+        if let Ok(bytes) = ser_bytes_result {
+            let mut m: BTreeMap<&str, &[u8]> = BTreeMap::new();
+            m.insert(MAP_KEY, &bytes);
+            if let Ok(mut conn) = self.get_connection() {
+                conn.xadd_maxlen_map(
+                    topics::SYNC_REPLY,
+                    StreamMaxlen::Approx(MAX_LEN),
+                    AUTO_ID,
+                    m,
+                )
+                .map_err(|e| XAddErr::Redis(e))
+            } else {
+                Err(XAddErr::Conn)
+            }
+        } else {
+            Err(XAddErr::Ser)
+        }
     }
 
     fn add_make_move(&self, data: MakeMove) -> Result<(), XAddErr> {
-        todo!()
+        let ser_bytes_result = bincode::serialize(&data);
+
+        if let Ok(bytes) = ser_bytes_result {
+            let mut m: BTreeMap<&str, &[u8]> = BTreeMap::new();
+            m.insert(MAP_KEY, &bytes);
+            if let Ok(mut conn) = self.get_connection() {
+                conn.xadd_maxlen_map(topics::MAKE_MOVE, StreamMaxlen::Approx(MAX_LEN), AUTO_ID, m)
+                    .map_err(|e| XAddErr::Redis(e))
+            } else {
+                Err(XAddErr::Conn)
+            }
+        } else {
+            Err(XAddErr::Ser)
+        }
     }
 }
