a81959973c288ddd91d485d7b4a2ef6ac282c135
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-29 10:48:10 -0400

    clean up create consumer group

--- micro-sync/src/stream/init.rs
+++ micro-sync/src/stream/init.rs
@@ -1,23 +1,16 @@
-use super::{topics, GROUP_NAME};
+use super::{topics::*, GROUP_NAME};
 use log::warn;
 use redis::Commands;
 pub fn create_consumer_group(client: &redis::Client) {
     let mut conn = client.get_connection().expect("group create conn");
-    let mm: Result<(), _> = conn.xgroup_create_mkstream(topics::PROVIDE_HISTORY, GROUP_NAME, "$");
-    if let Err(e) = mm {
-        warn!(
-            "Ignoring error creating {} consumer group (it probably exists already) {:?}",
-            topics::PROVIDE_HISTORY,
-            e
-        );
-    }
-    let gs: Result<(), _> =
-        conn.xgroup_create_mkstream(topics::GAME_STATES_CHANGELOG, GROUP_NAME, "$");
-    if let Err(e) = gs {
-        warn!(
-            "Ignoring error creating {} consumer group (it probably exists already) {:?}",
-            topics::GAME_STATES_CHANGELOG,
-            e
-        );
+    let to_create = vec![PROVIDE_HISTORY, GAME_STATES_CHANGELOG, REQ_SYNC, MOVE_MADE];
+    for topic in to_create {
+        let created: Result<(), _> = conn.xgroup_create_mkstream(topic, GROUP_NAME, "$");
+        if let Err(e) = created {
+            warn!(
+                "Ignoring error creating {} consumer group (it probably exists already) {:?}",
+                topic, e
+            );
+        }
     }
 }
