6a7d962941cb43916f1377fc0eab65a2b63195af
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-02 05:28:47 -0400

    add core structs

--- core-model/src/lib.rs
+++ core-model/src/lib.rs
@@ -1,7 +1,30 @@
+use serde_derive::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub struct GameId(pub Uuid);
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+impl GameId {
+    pub fn random() -> Self {
+        Self(Uuid::new_v4())
     }
 }
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub struct SessionId(pub Uuid);
+
+#[cfg(test)]
+impl SessionId {
+    pub fn random() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub struct ReqId(pub Uuid);
+#[cfg(test)]
+impl ReqId {
+    pub fn random() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct EventId(pub Uuid);
