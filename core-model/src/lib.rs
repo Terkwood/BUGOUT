3cef91c8e3118d910f36acb0c325928eb78e67c7
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-02 06:16:47 -0400

    define new

--- core-model/src/lib.rs
+++ core-model/src/lib.rs
@@ -3,28 +3,33 @@ use uuid::Uuid;
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct GameId(pub Uuid);
-#[cfg(test)]
+
 impl GameId {
-    pub fn random() -> Self {
+    pub fn now() -> Self {
         Self(Uuid::new_v4())
     }
 }
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct SessionId(pub Uuid);
 
-#[cfg(test)]
 impl SessionId {
-    pub fn random() -> Self {
+    pub fn new() -> Self {
         Self(Uuid::new_v4())
     }
 }
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct ReqId(pub Uuid);
-#[cfg(test)]
+
 impl ReqId {
-    pub fn random() -> Self {
+    pub fn new() -> Self {
         Self(Uuid::new_v4())
     }
 }
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct EventId(pub Uuid);
+
+impl EventId {
+    pub fn new() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
