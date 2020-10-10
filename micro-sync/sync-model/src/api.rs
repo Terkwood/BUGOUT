41b59ba006f1def6a04670eaaf113f458b1c72c7
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-10 17:10:03 -0400

    Add missing serdes (#351)

--- micro-sync/sync-model/src/api.rs
+++ micro-sync/sync-model/src/api.rs
@@ -13,7 +13,7 @@ pub struct ReqSync {
     pub last_move: Option<Move>,
 }
 
-#[derive(Clone, Debug, Serialize, PartialEq)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct SyncReply {
     pub session_id: SessionId,
     pub reply_to: ReqId,
@@ -23,13 +23,13 @@ pub struct SyncReply {
     pub moves: Vec<Move>,
 }
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ProvideHistory {
     pub game_id: GameId,
     pub req_id: ReqId,
 }
 
-#[derive(Clone, Debug, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct HistoryProvided {
     pub game_id: GameId,
     pub reply_to: ReqId,
