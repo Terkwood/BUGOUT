53e11a7dce535350a9133e968164aecdd3851fc0
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-10 15:41:40 -0400

    add dep

--- gateway/src/redis_io/into_shared.rs
+++ gateway/src/redis_io/into_shared.rs
@@ -1,7 +1,20 @@
 use crate::backend_commands::*;
+use crate::model::ProvideHistoryCommand;
 
 pub trait IntoShared<T> {
     fn into_shared(&self) -> T;
+}
+
+impl IntoShared<sync_model::api::ProvideHistory> for ProvideHistoryCommand {
+    fn into_shared(&self) -> sync_model::api::ProvideHistory {
+        todo!()
+    }
+}
+
+impl IntoShared<sync_model::api::ReqSync> for ReqSyncBackendCommand {
+    fn into_shared(&self) -> sync_model::api::ReqSync {
+        todo!()
+    }
 }
 
 impl IntoShared<lobby_model::api::JoinPrivateGame> for JoinPrivateGameBackendCommand {
@@ -21,6 +34,7 @@ impl IntoShared<lobby_model::api::FindPublicGame> for FindPublicGameBackendComma
         todo!()
     }
 }
+
 impl IntoShared<lobby_model::api::CreateGame> for CreateGameBackendCommand {
     fn into_shared(&self) -> lobby_model::api::CreateGame {
         todo!()
