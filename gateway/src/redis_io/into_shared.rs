49a3260c45b4e3aeca65024862d74ad05409b4dd
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-10 15:46:06 -0400

    abbrev & stub

--- gateway/src/redis_io/into_shared.rs
+++ gateway/src/redis_io/into_shared.rs
@@ -1,42 +1,54 @@
 use crate::backend_commands::*;
 use crate::model::ProvideHistoryCommand;
 
+use color_model as color;
+use core_model as core;
+use lobby_model as lobby;
+use sync_model as sync;
+
 pub trait IntoShared<T> {
     fn into_shared(&self) -> T;
 }
 
-impl IntoShared<sync_model::api::ProvideHistory> for ProvideHistoryCommand {
-    fn into_shared(&self) -> sync_model::api::ProvideHistory {
+impl IntoShared<sync::api::ProvideHistory> for ProvideHistoryCommand {
+    fn into_shared(&self) -> sync::api::ProvideHistory {
         todo!()
     }
 }
 
-impl IntoShared<sync_model::api::ReqSync> for ReqSyncBackendCommand {
-    fn into_shared(&self) -> sync_model::api::ReqSync {
+impl IntoShared<sync::api::ReqSync> for ReqSyncBackendCommand {
+    fn into_shared(&self) -> sync::api::ReqSync {
         todo!()
     }
 }
 
-impl IntoShared<lobby_model::api::JoinPrivateGame> for JoinPrivateGameBackendCommand {
-    fn into_shared(&self) -> lobby_model::api::JoinPrivateGame {
-        use core_model as cm;
-
-        lobby_model::api::JoinPrivateGame {
-            game_id: cm::GameId(self.game_id),
-            client_id: cm::ClientId(self.client_id),
-            session_id: cm::SessionId(self.session_id),
+impl IntoShared<lobby::api::JoinPrivateGame> for JoinPrivateGameBackendCommand {
+    fn into_shared(&self) -> lobby::api::JoinPrivateGame {
+        lobby::api::JoinPrivateGame {
+            game_id: core::GameId(self.game_id),
+            client_id: core::ClientId(self.client_id),
+            session_id: core::SessionId(self.session_id),
         }
     }
 }
 
-impl IntoShared<lobby_model::api::FindPublicGame> for FindPublicGameBackendCommand {
-    fn into_shared(&self) -> lobby_model::api::FindPublicGame {
+impl IntoShared<lobby::api::FindPublicGame> for FindPublicGameBackendCommand {
+    fn into_shared(&self) -> lobby::api::FindPublicGame {
+        lobby::api::FindPublicGame {
+            client_id: core::ClientId(self.client_id),
+            session_id: core::SessionId(self.session_id),
+        }
+    }
+}
+
+impl IntoShared<lobby::api::CreateGame> for CreateGameBackendCommand {
+    fn into_shared(&self) -> lobby::api::CreateGame {
         todo!()
     }
 }
 
-impl IntoShared<lobby_model::api::CreateGame> for CreateGameBackendCommand {
-    fn into_shared(&self) -> lobby_model::api::CreateGame {
+impl IntoShared<color::api::ChooseColorPref> for ChooseColorPrefBackendCommand {
+    fn into_shared(&self) -> color::api::ChooseColorPref {
         todo!()
     }
 }
