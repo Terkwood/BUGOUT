792001858eb15e5f22bcd97d82f5992426c402c1
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-13 18:48:51 -0400

    implement xread

--- gateway/src/redis_io/xread.rs
+++ gateway/src/redis_io/xread.rs
@@ -107,52 +107,56 @@ fn deser(xread_result: XReadResult) -> HashMap<XReadEntryId, StreamData> {
                     }
                 }
             } else if &xread_topic[..] == topics::HISTORY_PROVIDED_TOPIC {
-                for with_timestamps in xread_data {
-                    for (k, v) in with_timestamps {
-                        let shape: Result<(String, Option<Vec<u8>>), _> = // data <bin> 
-                            redis::FromRedisValue::from_redis_value(&v);
-                        if let Ok(s) = shape {
-                            if let (Ok(xid), Some(hist_prov)) = (
-                                XReadEntryId::from_str(k),
-                                bincode::deserialize::<sync_model::api::HistoryProvided>(
-                                    &s.1.unwrap_or_default(),
-                                )
-                                .ok(),
-                            ) {
-                                stream_data.insert(xid, StreamData::HistoryProvided(hist_prov));
-                            } else {
-                                warn!("Xread: Deser error   hist prov data")
-                            }
-                        } else {
-                            error!("Fail XREAD hist prov")
-                        }
-                    }
-                }
+                bin_data_process(
+                    xread_data,
+                    &mut stream_data,
+                    Box::new(|bytes| {
+                        bincode::deserialize::<sync_model::api::HistoryProvided>(&bytes)
+                    }),
+                    topics::HISTORY_PROVIDED_TOPIC,
+                )
             } else if &xread_topic[..] == topics::SYNC_REPLY_TOPIC {
                 bin_data_process(
                     xread_data,
                     &mut stream_data,
                     Box::new(|bytes| bincode::deserialize::<sync_model::api::SyncReply>(&bytes)),
+                    topics::SYNC_REPLY_TOPIC,
                 )
             } else if &xread_topic[..] == topics::WAIT_FOR_OPPONENT_TOPIC {
-                todo!()
+                bin_data_process(
+                    xread_data,
+                    &mut stream_data,
+                    Box::new(|bytes| {
+                        bincode::deserialize::<lobby_model::api::WaitForOpponent>(&bytes)
+                    }),
+                    topics::WAIT_FOR_OPPONENT_TOPIC,
+                )
+            } else if &xread_topic[..] == topics::GAME_READY_TOPIC {
+                bin_data_process(
+                    xread_data,
+                    &mut stream_data,
+                    Box::new(|bytes| bincode::deserialize::<lobby_model::api::GameReady>(&bytes)),
+                    topics::GAME_READY_TOPIC,
+                )
+            } else if &xread_topic[..] == topics::PRIVATE_GAME_REJECTED_TOPIC {
+                bin_data_process(
+                    xread_data,
+                    &mut stream_data,
+                    Box::new(|bytes| {
+                        bincode::deserialize::<lobby_model::api::PrivateGameRejected>(&bytes)
+                    }),
+                    topics::PRIVATE_GAME_REJECTED_TOPIC,
+                )
+            } else if &xread_topic[..] == topics::COLORS_CHOSEN_TOPIC {
+                bin_data_process(
+                    xread_data,
+                    &mut stream_data,
+                    Box::new(|bytes| {
+                        bincode::deserialize::<color_model::api::ColorsChosen>(&bytes)
+                    }),
+                    topics::COLORS_CHOSEN_TOPIC,
+                )
             } else {
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-                // TODO// TODO// TODO// TODO// TODO// TODO
-
                 warn!("Ignoring topic {}", &xread_topic[..])
             }
         }
@@ -165,6 +169,7 @@ fn bin_data_process<'a, T>(
     xread_data: &Vec<HashMap<String, redis::Value, std::collections::hash_map::RandomState>>,
     stream_data: &mut HashMap<XReadEntryId, StreamData>,
     des: Box<dyn Fn(Vec<u8>) -> Result<T, Box<bincode::ErrorKind>>>,
+    topic: &str,
 ) where
     T: Deserialize<'a> + std::convert::Into<crate::redis_io::stream::StreamData>,
 {
@@ -178,16 +183,41 @@ fn bin_data_process<'a, T>(
                 {
                     stream_data.insert(xid, local.into());
                 } else {
-                    warn!("Xread: Deser error  sync reply data")
+                    warn!("Xread: Deser error  {}", topic)
                 }
             } else {
-                error!("Fail XREAD sync reply")
+                error!("Fail XREAD {}", topic)
             }
         }
+    }
+}
+impl From<sync_model::api::HistoryProvided> for StreamData {
+    fn from(a: sync_model::api::HistoryProvided) -> Self {
+        StreamData::HistoryProvided(a)
     }
 }
 impl From<sync_model::api::SyncReply> for StreamData {
     fn from(h: sync_model::api::SyncReply) -> Self {
         StreamData::SyncReply(h)
+    }
+}
+impl From<lobby_model::api::WaitForOpponent> for StreamData {
+    fn from(w: lobby_model::api::WaitForOpponent) -> Self {
+        StreamData::WaitForOpponent(w)
+    }
+}
+impl From<lobby_model::api::GameReady> for StreamData {
+    fn from(w: lobby_model::api::GameReady) -> Self {
+        StreamData::GameReady(w)
+    }
+}
+impl From<lobby_model::api::PrivateGameRejected> for StreamData {
+    fn from(w: lobby_model::api::PrivateGameRejected) -> Self {
+        StreamData::PrivGameRejected(w)
+    }
+}
+impl From<color_model::api::ColorsChosen> for StreamData {
+    fn from(w: color_model::api::ColorsChosen) -> Self {
+        StreamData::ColorsChosen(w)
     }
 }
