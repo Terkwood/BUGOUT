6e5474c2a203d2be0f0521b06b796803bce23d0c
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-13 16:58:15 -0400

    update color-chooser model (#355)

--- micro-color-chooser/color-model/src/api.rs
+++ micro-color-chooser/color-model/src/api.rs
@@ -5,14 +5,14 @@ use serde_derive::{Deserialize, Serialize};
 /// emitted by the game lobby
 pub use lobby_model::api::GameReady;
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ChooseColorPref {
     pub client_id: ClientId,
     pub color_pref: ColorPref,
     pub session_id: SessionId,
 }
 
-#[derive(Clone, Debug, Serialize, PartialEq)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 pub struct ColorsChosen {
     pub game_id: GameId,
     pub black: ClientId,
