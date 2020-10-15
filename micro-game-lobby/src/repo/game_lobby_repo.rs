012fc98ffc58e4dd5b00dce859cf312f2b94ab14
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-15 15:15:45 -0400

    rewrite fn

--- micro-game-lobby/src/repo/game_lobby_repo.rs
+++ micro-game-lobby/src/repo/game_lobby_repo.rs
@@ -12,11 +12,16 @@ pub trait GameLobbyRepo {
 impl GameLobbyRepo for Rc<Client> {
     fn get(&self) -> Result<GameLobby, FetchErr> {
         if let Ok(mut conn) = self.get_connection() {
-            let data: Result<Vec<u8>, _> = conn.get(super::GAME_LOBBY_KEY).map_err(|_| FetchErr);
-
-            data.and_then(|bytes| bincode::deserialize(&bytes).map_err(|_| FetchErr))
+            let data: Result<Option<Vec<u8>>, _> = conn
+                .get(super::GAME_LOBBY_KEY)
+                .map_err(|_| FetchErr::RedisCall);
+            match data {
+                Ok(Some(bytes)) => Ok(bincode::deserialize(&bytes)?),
+                Ok(None) => Ok(GameLobby::default()),
+                Err(_) => Err(FetchErr::RedisCall),
+            }
         } else {
-            Err(FetchErr)
+            Err(FetchErr::Conn)
         }
     }
     fn put(&self, game_lobby: GameLobby) -> Result<(), WriteErr> {
--- micro-game-lobby/src/repo/mod.rs
+++ micro-game-lobby/src/repo/mod.rs
@@ -2,9 +2,19 @@ mod game_lobby_repo;
 pub use game_lobby_repo::*;
 
 #[derive(Debug)]
-pub struct FetchErr;
+pub enum FetchErr {
+    Deser,
+    RedisCall,
+    Conn,
+}
 
 #[derive(Debug)]
 pub struct WriteErr;
 
 pub const GAME_LOBBY_KEY: &str = "/BUGOUT/micro_game_lobby/game_lobby";
+
+impl From<Box<bincode::ErrorKind>> for FetchErr {
+    fn from(_: Box<bincode::ErrorKind>) -> Self {
+        FetchErr::Deser
+    }
+}
