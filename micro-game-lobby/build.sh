32fb66122c459fada0dc022274a0252f24375395
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-16 13:19:21 -0400

    hack dc

--- micro-game-lobby/build.sh
+++ micro-game-lobby/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DOCKER_BUILDKIT=1 docker build -t bugout_micro-game-lobby:latest .
+DOCKER_BUILDKIT=1 docker build --build-arg BUILDKIT_INLINE_CACHE=1 -t bugout_micro-game-lobby:latest .
