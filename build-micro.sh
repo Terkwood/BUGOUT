edb893d3221866167057b47e8721ca37314a8300
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-16 20:50:37 -0400

    Finalize builds

--- build-micro.sh
+++ build-micro.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sh cargo-clean-build.sh gateway
-sh cargo-clean-build.sh micro-judge
-sh cargo-clean-build.sh micro-changelog
+sh compose.sh build micro-judge
+sh compose.sh build micro-changelog
 sh compose.sh build micro-game-lobby
-sh compose.sh micro-color-chooser
-sh compose.sh micro-sync
+sh compose.sh build micro-color-chooser
+sh compose.sh build micro-sync
