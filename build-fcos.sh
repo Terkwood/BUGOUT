3e9b8801872757d47d450dd32da813e5e38ec343
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-20 14:34:09 -0400

    Update admin build script

--- build-fcos.sh
+++ build-fcos.sh
@@ -2,10 +2,10 @@
 
 export COMPOSE_DOCKER_CLI_BUILD=1 
 export DOCKER_BUILDKIT=1
-/opt/bin/docker-compose build gateway
-/opt/bin/docker-compose build micro-judge
-/opt/bin/docker-compose build micro-changelog
-/opt/bin/docker-compose build micro-game-lobby
-/opt/bin/docker-compose build micro-color-chooser
-/opt/bin/docker-compose build micro-sync
-/opt/bin/docker-compose build botlink
+docker-compose build gateway
+docker-compose build micro-judge
+docker-compose build micro-changelog
+docker-compose build micro-game-lobby
+docker-compose build micro-color-chooser
+docker-compose build micro-sync
+docker-compose build botlink
