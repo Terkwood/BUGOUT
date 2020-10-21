20bdb37d2deb2f42ddbe4bb00e396e8d261fa771
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-21 11:09:21 -0400

    alter default botlink address, touch up dc.yml

--- botlink/src/env.rs
+++ botlink/src/env.rs
@@ -3,7 +3,7 @@ use std::env;
 const ENV_AUTHORIZATION: &str = "AUTHORIZATION"; // username:password
 const ENV_ADDRESS: &str = "ADDRESS";
 
-const DEFAULT_ADDRESS: &str = "127.0.0.1:3012";
+const DEFAULT_ADDRESS: &str = "0.0.0.0:3012";
 lazy_static! {
     pub static ref AUTHORIZATION: Option<String> = env::var(ENV_AUTHORIZATION).ok();
     pub static ref ADDRESS: String = env::var(ENV_ADDRESS).unwrap_or(DEFAULT_ADDRESS.to_string());
