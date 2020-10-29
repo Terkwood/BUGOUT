ec527d1c1a8836fb75e94cb4691dd77f6ce36437
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-29 09:35:26 -0400

    half-impl repo

--- botlink/src/repo/difficulty.rs
+++ botlink/src/repo/difficulty.rs
@@ -1,10 +1,27 @@
-use super::RepoErr;
+use super::{expire, RepoErr};
 use bot_model::Difficulty;
 use core_model::GameId;
+use redis::{Client, Commands};
 
 pub trait DifficultyRepo: Send + Sync {
     fn get(&self, game_id: &GameId) -> Result<Option<Difficulty>, RepoErr>;
     fn put(&self, game_id: &GameId, difficulty: Difficulty) -> Result<(), RepoErr>;
+}
+
+impl DifficultyRepo for Box<Client> {
+    fn get(&self, game_id: &GameId) -> Result<Option<Difficulty>, RepoErr> {
+        if let Ok(mut conn) = self.get_connection() {
+            let ser: Option<Vec<u8>> = conn.get(difficulty_key(&game_id))?;
+            expire(&difficulty_key(game_id), &mut conn)?;
+            Ok(todo!())
+        } else {
+            Err(RepoErr::Conn)
+        }
+    }
+
+    fn put(&self, game_id: &GameId, difficulty: Difficulty) -> Result<(), RepoErr> {
+        todo!()
+    }
 }
 
 fn difficulty_key(game_id: &GameId) -> String {
