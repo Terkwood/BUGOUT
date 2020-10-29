973ce871b661bdc66f7c7b07b3d4e74fd6213c15
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-10-29 13:53:39 -0400

    Restore model

--- botlink/bot-model/src/lib.rs
+++ botlink/bot-model/src/lib.rs
@@ -13,17 +13,9 @@ pub enum Difficulty {
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct AlphaNumCoord(pub char, pub u16);
 
-mod tests {
-    #[test]
-    fn test_difficulty_json() {
-        let input = Difficulty::Max;
-        let json = serde_json::to_string(&input).expect("to_string");
-        assert_eq!(json, "\"Max\"")
-    }
-
-    #[test]
-    fn test_difficulty_bincode() {
-        let input = Difficulty::Easy;
-        let bytes = bincode::serialize(&difficulty)?;
-    }
+#[test]
+fn test_difficulty_json() {
+    let input = Difficulty::Max;
+    let json = serde_json::to_string(&input).expect("to_string");
+    assert_eq!(json, "\"Max\"")
 }
