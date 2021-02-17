2b1b6760926137470682e740b352be88d74f7717
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-02-17 15:21:42 -0500

    reload on click

--- browser/src/components/bugout/InvalidLinkModal.js
+++ browser/src/components/bugout/InvalidLinkModal.js
@@ -6,40 +6,32 @@ import Dialog from "preact-material-components/Dialog";
 class InvalidLinkModal extends Component {
   constructor() {
     super();
-    this.state = { showDialog: false };
   }
 
   render({ id = "invalid-link-modal" }) {
-    let { showDialog } = this.state;
-
-    let empty = h("div", { id });
-
-    return showDialog
-      ? h(
-          Dialog,
+    return h(
+      Dialog,
+      {
+        id,
+        isOpen: true,
+      },
+      h(Dialog.Header, null, "Invalid Link"),
+      h(Dialog.Body, null, "This link is no longer available."),
+      h(
+        Dialog.Footer,
+        null,
+        h(
+          Dialog.FooterButton,
           {
-            id,
-            isOpen: true,
+            accept: true,
+            onClick: () => {
+              location.reload();
+            },
           },
-          h(Dialog.Header, null, "Invalid Link"),
-          h(Dialog.Body, null, "This link is no longer available."),
-          h(
-            Dialog.Footer,
-            null,
-            h(
-              Dialog.FooterButton,
-              {
-                accept: true,
-                onClick: () => {
-                  this.setState({ showDialog: false });
-                  location.reload();
-                },
-              },
-              "START OVER"
-            )
-          )
+          "START OVER"
         )
-      : empty;
+      )
+    );
   }
 }
 
