050bceac83d925f50687561417bcc651bb7d7e0f
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-04-26 16:44:43 -0400

    wire up undo, address example lint

--- gateway/examples/encode_uuid.rs
+++ gateway/examples/encode_uuid.rs
@@ -2,12 +2,10 @@ extern crate gateway;
 extern crate uuid;
 use gateway::compact_ids::CompactId;
 
-const FAIL_MSG: &str = "You must specify a UUID as a command line argument";
-
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
-        panic!(FAIL_MSG)
+        panic!("You must specify a UUID as a command line argument")
     }
 
     let a = args.get(1).unwrap();
@@ -15,6 +13,6 @@ fn main() {
     if let Ok(u) = uuid::Uuid::parse_str(a) {
         println!("{}", CompactId::encode(u).0)
     } else {
-        panic!(FAIL_MSG)
+        panic!("You must specify a UUID as a command line argument")
     }
 }
