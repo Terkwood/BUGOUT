3ad089b55a86e354f0ad1fac1c94d07566fa7104
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-04-27 14:41:34 -0400

    fix: use camelCase in undo API

--- undo/undo-model/src/api.rs
+++ undo/undo-model/src/api.rs
@@ -3,12 +3,14 @@ use move_model::{GameState, Player};
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub struct UndoMove {
     pub game_id: GameId,
     pub player: Player,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct MoveUndone {
     pub game_id: GameId,
     pub player: Player,
