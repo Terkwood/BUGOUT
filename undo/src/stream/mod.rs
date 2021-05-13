bc1e1567529024f9896da51bd5a667b71b1cf25f
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-05-13 15:46:40 -0400

    trim

--- undo/src/stream/mod.rs
+++ undo/src/stream/mod.rs
@@ -2,27 +2,24 @@ mod handlers;
 mod topics;
 mod undo;
 mod xadd;
-mod xread;
 
 pub use handlers::init;
 pub use xadd::*;
-pub use xread::*;
 
 use bot_model::api::*;
 use move_model::GameState;
 use undo_model::api::*;
 
-
 #[derive(Debug, Clone, PartialEq)]
 pub enum StreamOutput {
-    MU(MoveUndone),
-    LOG(GameState),
-    REJECT(UndoMove),
+  MU(MoveUndone),
+  LOG(GameState),
+  REJECT(UndoMove),
 }
 
 #[derive(Clone, Debug)]
 pub enum StreamInput {
-    UM(UndoMove),
-    LOG(GameState),
-    BA(BotAttached),
+  UM(UndoMove),
+  LOG(GameState),
+  BA(BotAttached),
 }
