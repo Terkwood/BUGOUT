1d177cc5708a22bb68a572461dc466d25e3c4d74
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-05-27 17:24:20 -0400

    add test, and part of a method

--- redis_streams/src/sorted_streams.rs
+++ redis_streams/src/sorted_streams.rs
@@ -2,11 +2,28 @@ use crate::consumer_group::{ConsumerGroup, Message};
 use anyhow::{Context, Result};
 
 trait SortedStreams {
-    fn consume() -> Result<()>;
+    fn consume(&self) -> Result<()>;
 }
 
-impl<'a, F> SortedStreams for Vec<ConsumerGroup<'a, F>> where F: FnMut(&str, &Message) -> Result<()> {
-    fn consume() -> Result<()> {
+impl<'a, F> SortedStreams for Vec<ConsumerGroup<'a, F>>
+where
+    F: FnMut(&str, &Message) -> Result<()>,
+{
+    fn consume(&self) -> Result<()> {
+        for _consumer_group in self {
+            todo!()
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_consume() {
         todo!()
     }
 }
