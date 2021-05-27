cef00939eee9acdf1954283832cc31b211ec9532
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-05-27 15:53:50 -0400

    make a trait

--- redis_streams/src/sorted_streams.rs
+++ redis_streams/src/sorted_streams.rs
@@ -1,1 +1,12 @@
-pub struct SortedStreams {}
+use crate::consumer_group::{ConsumerGroup, Message};
+use anyhow::{Context, Result};
+
+trait SortedStreams {
+    fn consume() -> Result<()>;
+}
+
+impl<'a, F> SortedStreams for Vec<ConsumerGroup<'a, F>> where F: FnMut(&str, &Message) -> Result<()> {
+    fn consume() -> Result<()> {
+        todo!()
+    }
+}
