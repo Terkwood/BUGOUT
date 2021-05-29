63a69013dd736fe478961d285c30c435f39ff5fd
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-05-29 13:28:28 -0400

    trim

--- micro-game-lobby/src/components.rs
+++ micro-game-lobby/src/components.rs
@@ -1,6 +1,5 @@
 use crate::repo::GameLobbyRepo;
 use crate::stream::XAdd;
-use redis_streams::{RedisSortedStreams, SortedStreams};
 use std::rc::Rc;
 
 pub struct Components {
@@ -14,13 +13,11 @@ pub fn redis_client() -> Rc<redis::Client> {
     Rc::new(redis::Client::open(REDIS_URL).expect("redis client"))
 }
 
-
 impl Components {
     pub fn new(client: Rc<redis::Client>) -> Self {
-        
         Components {
             game_lobby_repo: Box::new(client.clone()),
-            xadd: Box::new(client), 
+            xadd: Box::new(client),
         }
     }
 }
