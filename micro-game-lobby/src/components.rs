2420bdd686b65a0961082660e8ee3442e07c69e9
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-05-29 09:48:56 -0400

    stub out components

--- micro-game-lobby/src/components.rs
+++ micro-game-lobby/src/components.rs
@@ -17,10 +17,27 @@ pub fn redis_client() -> Rc<redis::Client> {
 
 impl Components {
     pub fn new(client: Rc<redis::Client>) -> Self {
+        let mut conn = client.get_connection().expect("redis conn");
+        let stream_handlers: Vec<(
+            &str,
+            Box<
+                FnMut(
+                    redis_streams::XId,
+                    &redis_streams::Message,
+                ) -> Result<(), redis_streams::anyhow::Error>,
+            >,
+        )> = vec![
+            ("some-stream", todo!()),
+            ("another-stream", todo!()),
+            ("fix-the-names", todo!()),
+        ];
+        let sorted_streams =
+            RedisSortedStreams::xgroup_create_mkstreams(stream_handlers, todo!("opts"), &mut conn)
+                .expect("stream creation");
         Components {
             game_lobby_repo: Box::new(client.clone()),
             xadd: Box::new(client),
-            sorted_streams: todo!(),
+            sorted_streams: Box::new(sorted_streams),
         }
     }
 }
